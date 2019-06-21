3c9729fbd7a4a26085d36fe62850a5a4f5d6e645
Matous Jobanek <mjobanek@redhat.com>
2019-06-21 15:54:01 +0200

    feat(CRT-145): sync MasterUserRecord status with UserAccounts in members

--- pkg/controller/masteruserrecord/masteruserrecord_controller.go
+++ pkg/controller/masteruserrecord/masteruserrecord_controller.go
@@ -2,12 +2,15 @@ package masteruserrecord
 
 import (
 	"context"
-
+	"fmt"
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-
-	corev1 "k8s.io/api/core/v1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
+	"github.com/go-logr/logr"
+	"github.com/operator-framework/operator-sdk/pkg/predicate"
 	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -15,6 +18,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sigs.k8s.io/kubefed/pkg/controller/util"
 )
 
 var log = logf.Log.WithName("controller_masteruserrecord")
@@ -32,7 +36,10 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileMasterUserRecord{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	return &ReconcileMasterUserRecord{
+		client:           mgr.GetClient(),
+		scheme:           mgr.GetScheme(),
+		getMemberCluster: cluster.GetFedCluster}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
@@ -44,17 +51,10 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	}
 
 	// Watch for changes to primary resource MasterUserRecord
-	err = c.Watch(&source.Kind{Type: &toolchainv1alpha1.MasterUserRecord{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner MasterUserRecord
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &toolchainv1alpha1.MasterUserRecord{},
-	})
+	err = c.Watch(&source.Kind{
+		Type: &toolchainv1alpha1.MasterUserRecord{}},
+		&handler.EnqueueRequestForObject{},
+		predicate.GenerationChangedPredicate{})
 	if err != nil {
 		return err
 	}
@@ -68,8 +68,9 @@ var _ reconcile.Reconciler = &ReconcileMasterUserRecord{}
 type ReconcileMasterUserRecord struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
-	client client.Client
-	scheme *runtime.Scheme
+	client           client.Client
+	scheme           *runtime.Scheme
+	getMemberCluster func(name string) (*cluster.FedCluster, bool)
 }
 
 // Reconcile reads that state of the cluster for a MasterUserRecord object and makes changes based on the state read
@@ -81,9 +82,9 @@ func (r *ReconcileMasterUserRecord) Reconcile(request reconcile.Request) (reconc
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling MasterUserRecord")
 
-	// Fetch the MasterUserRecord instance
-	instance := &toolchainv1alpha1.MasterUserRecord{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	// Fetch the MasterUserRecord userRecord
+	userRecord := &toolchainv1alpha1.MasterUserRecord{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, userRecord)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
@@ -95,6 +96,98 @@ func (r *ReconcileMasterUserRecord) Reconcile(request reconcile.Request) (reconc
 		return reconcile.Result{}, err
 	}
 
-	// Do nothing for now
+	// check if the user was already approved
+	if userRecord.Spec.State == "pending" { // TODO add statuses to MasterUserRecord in API repo
+		reqLogger.Info("The user hasn't been approved yet")
+		return reconcile.Result{}, nil
+	}
+
+	for _, account := range userRecord.Spec.UserAccounts {
+		err = r.ensureUserAccount(reqLogger, account, userRecord)
+		if err != nil {
+			reqLogger.Error(err, "unable to synchronize with member UserAccount")
+			return reconcile.Result{}, err
+		}
+	}
+
 	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileMasterUserRecord) ensureUserAccount(log logr.Logger, recAccount toolchainv1alpha1.UserAccountEmbedded, record *toolchainv1alpha1.MasterUserRecord) error {
+	// get & check fed cluster
+	fedCluster, ok := r.getMemberCluster(recAccount.TargetCluster)
+	if !ok {
+		return fmt.Errorf("the fedCluster %s not found in the registry", recAccount.TargetCluster)
+	}
+	if !util.IsClusterReady(fedCluster.ClusterStatus) {
+		return fmt.Errorf("the fedCluster %s is not ready", recAccount.TargetCluster)
+	}
+
+	// get UserAccount from member
+	nsdName := namespacedName(fedCluster.OperatorNamespace, record.Name)
+	userAccount := &toolchainv1alpha1.UserAccount{}
+	err := fedCluster.Client.Get(context.TODO(), nsdName, userAccount)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// does not exist - should create
+			userAccount = newUserAccount(nsdName, recAccount.Spec)
+			if err := updateStatus(r.client, record, "provisioning", ""); err != nil {
+				return err
+			}
+			if err := fedCluster.Client.Create(context.TODO(), userAccount); err != nil {
+				return err
+			}
+			return nil
+		}
+		if updateErr := updateStatus(r.client, record, "provisioning", ""); updateErr != nil {
+			log.Error(updateErr, "unable to update status")
+		}
+		return err
+	}
+
+	sync := Synchronizer{
+		record:        record,
+		hostClient:    r.client,
+		memberClient:  fedCluster.Client,
+		memberUserAcc: userAccount,
+		recordUserAcc: recAccount,
+	}
+	if err := sync.synchronizeSpec(); err != nil {
+		return err
+	}
+	if err := sync.synchronizeStatus(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// updateStatus updates user account status to given status with errMsg but only if the current status doesn't match
+// If the current status already set to desired state then this method does nothing
+// TODO the status should be a specific type of a string or StatusUserAccount - same as for UserAccount
+func updateStatus(client client.Client, record *toolchainv1alpha1.MasterUserRecord, status, errMsg string) error {
+	// TODO we need to add record.Status.Error field to have place where to store internal errors
+	if record.Status.Status == status { //&& record.Status.Error == errMsg {
+		// Nothing changed
+		return nil
+	}
+	record.Status = toolchainv1alpha1.MasterUserRecordStatus{
+		Status: status,
+		//Error:  errMsg,
+	}
+	return client.Status().Update(context.TODO(), record)
+}
+
+func newUserAccount(nsdName types.NamespacedName, spec toolchainv1alpha1.UserAccountSpec) *toolchainv1alpha1.UserAccount {
+	return &toolchainv1alpha1.UserAccount{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      nsdName.Name,
+			Namespace: nsdName.Namespace,
+		},
+		Spec: spec,
+	}
+}
+
+func namespacedName(namespace, name string) types.NamespacedName {
+	return types.NamespacedName{Namespace: namespace, Name: name}
 }
---
+++ pkg/controller/masteruserrecord/sync.go
@@ -0,0 +1,74 @@
+package masteruserrecord
+
+import (
+	"context"
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Synchronizer struct {
+	hostClient    client.Client
+	memberClient  client.Client
+	memberUserAcc *toolchainv1alpha1.UserAccount
+	recordUserAcc toolchainv1alpha1.UserAccountEmbedded
+	record        *toolchainv1alpha1.MasterUserRecord
+}
+
+func (s *Synchronizer) synchronizeSpec() error {
+	if !reflect.DeepEqual(s.memberUserAcc.Spec, s.recordUserAcc.Spec) { // TODO maybe do not use reflect :-)
+		// when UserAccount spec in record is updated - is not same as in member
+		s.memberUserAcc.Spec = s.recordUserAcc.Spec
+		if err := updateStatus(s.hostClient, s.record, "updating", ""); err != nil {
+			return err
+		}
+		err := s.memberClient.Update(context.TODO(), s.memberUserAcc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Synchronizer) synchronizeStatus() error {
+	recAccStatus, index := getUserAccountStatus(s.recordUserAcc.TargetCluster, s.record)
+	if index < 0 || s.recordUserAcc.SyncIndex == recAccStatus.SyncIndex {
+		// when record should update status
+		recAccStatus.SyncIndex = s.recordUserAcc.SyncIndex
+		recAccStatus.UserAccountStatus = s.memberUserAcc.Status
+		if index < 0 {
+			s.record.Status.UserAccounts = append(s.record.Status.UserAccounts, recAccStatus)
+		} else {
+			s.record.Status.UserAccounts[index] = recAccStatus
+		}
+
+		s.checkStatus("provisioning", "provisioned")
+		s.checkStatus("updating", "provisioned")
+
+		return s.hostClient.Update(context.TODO(), s.record)
+	}
+	return nil
+}
+
+// checkStatus checks if all embedded SAs has finished the ongoing job (has provisioned/updated)
+func (s *Synchronizer) checkStatus(currentStatus, followingStatus string) {
+	if s.record.Status.Status == currentStatus {
+		for _, ua := range s.record.Status.UserAccounts {
+			if string(ua.Status) != followingStatus {
+				return
+			}
+		}
+		s.record.Status.Status = followingStatus
+	}
+}
+
+func getUserAccountStatus(clusterName string, record *toolchainv1alpha1.MasterUserRecord) (toolchainv1alpha1.UserAccountStatusEmbedded, int) {
+	for i, account := range record.Status.UserAccounts {
+		if account.TargetCluster == clusterName {
+			return account, i
+		}
+	}
+	return toolchainv1alpha1.UserAccountStatusEmbedded{
+		TargetCluster: clusterName,
+	}, -1
+}
