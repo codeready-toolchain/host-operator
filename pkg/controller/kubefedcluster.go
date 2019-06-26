aecd8c4d0a35d67146449ed3cec47fe05666ce19
Matous Jobanek <matousjobanek@gmail.com>
2019-06-26 11:08:30 +0200

    feat(CRT-150): add caching controller for KubeFedCluster (#14)

--- pkg/controller/kubefedcluster.go
+++ pkg/controller/kubefedcluster.go
@@ -1,8 +1,8 @@
 package controller
 
-
 import (
 	"fmt"
+	"github.com/codeready-toolchain/toolchain-common/pkg/controller"
 	"k8s.io/klog"
 	"os"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -12,7 +12,17 @@ import (
 
 const varOperatorNamespace = "OPERATOR_NAMESPACE"
 
-func registerKubeFedClusterController(mgr manager.Manager, stopChan <-chan struct{}) error {
+func StartKubeFedClusterControllers(mgr manager.Manager, stopChan <-chan struct{}) error {
+	if err := startHealthCheckController(mgr, stopChan); err != nil {
+		return err
+	}
+	if err := controller.StartCachingController(mgr, stopChan); err != nil {
+		return err
+	}
+	return nil
+}
+
+func startHealthCheckController(mgr manager.Manager, stopChan <-chan struct{}) error {
 	ns, found := os.LookupEnv(varOperatorNamespace)
 	if !found {
 		return fmt.Errorf("%s must be set", varOperatorNamespace)
