aff78c139a28945827e7b3ca21b138720191b9f7
Dipak Pawar <dipakpawar231@gmail.com>
2019-07-31 18:03:40 +0530

    fix typo

--- make/deploy.mk
+++ make/deploy.mk
@@ -1,5 +1,5 @@
 
 .PHONY: deploy-host
-## Run Operator locally
+## Deploy Operator on dev cluster
 deploy-host: create-namespace deploy-rbac deploy-crd
 	oc apply -f deploy/operator_dev_cluster.yaml
