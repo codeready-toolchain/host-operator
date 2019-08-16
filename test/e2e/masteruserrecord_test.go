9d1df562b022fdf01ad2daada10a7588ee71ab73
Matous Jobanek <mjobanek@redhat.com>
2019-08-16 10:02:27 +0200

    e2e tests

--- test/e2e/masteruserrecord_test.go
+++ test/e2e/masteruserrecord_test.go
@@ -5,6 +5,7 @@ import (
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	murtest "github.com/codeready-toolchain/host-operator/test/masteruserrecord"
 	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
+	"github.com/codeready-toolchain/toolchain-common/pkg/condition"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test/e2e"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/stretchr/testify/assert"
@@ -29,28 +30,87 @@ func TestMasterUserRecord(t *testing.T) {
 
 	// then
 	require.NoError(t, err)
-	t.Logf("user account '%s' created", mur.Name)
+	t.Logf("MasterUserRecord '%s' created", mur.Name)
 
 	err = verifyResources(awaitility, mur, toBeProvisioned())
 	assert.NoError(t, err)
 
 	err = verifyResources(awaitility, extraMur, toBeProvisioned())
 	assert.NoError(t, err)
+
+	t.Run("update UserAccount spec when MasterUserRecord spec is modified", func(t *testing.T) {
+		// given
+		toBeModifiedMur := NewHostAwaitility(awaitility).GetMasterUserRecord(extraMur.Name)
+		murtest.ModifyUaInMur(toBeModifiedMur, targetCluster, murtest.NsLimit("advanced"),
+			murtest.TierName("admin"), murtest.Namespace("che", "4321"))
+
+		// when
+		err := awaitility.Client.Update(context.TODO(), toBeModifiedMur)
+
+		// then
+		require.NoError(t, err)
+
+		t.Logf("MasterUserRecord '%s' updated", mur.Name)
+
+		err = verifyResources(awaitility, toBeModifiedMur, toBeProvisioned())
+		assert.NoError(t, err)
+		err = verifyResources(awaitility, mur, toBeProvisioned())
+		assert.NoError(t, err)
+	})
+
+	t.Run("update MasterUserRecord status when UserAccount status is modified", func(t *testing.T) {
+		// given
+		currentMur := NewHostAwaitility(awaitility).GetMasterUserRecord(mur.Name)
+		userAccount := NewMemberAwaitility(awaitility).GetUserAccount(mur.Name)
+		userAccount.Status.Conditions, _ = condition.AddOrUpdateStatusConditions(
+			userAccount.Status.Conditions, coolStatus())
+
+		// when
+		err := awaitility.ControllerClient.Status().Update(context.TODO(), userAccount)
+
+		// then
+		require.NoError(t, err)
+
+		t.Logf("MasterUserRecord '%s' updated", mur.Name)
+
+		err = verifyResources(awaitility, currentMur, toBeProvisioned(), coolStatus())
+		assert.NoError(t, err)
+		extraMur = NewHostAwaitility(awaitility).GetMasterUserRecord(extraMur.Name)
+		err = verifyResources(awaitility, extraMur, toBeProvisioned())
+		assert.NoError(t, err)
+	})
 }
 
-func verifyResources(awaitility *e2e.Awaitility, mur *toolchainv1alpha1.MasterUserRecord, murCond toolchainv1alpha1.Condition) error {
+func verifyResources(awaitility *e2e.Awaitility, mur *toolchainv1alpha1.MasterUserRecord, uaStatusConds ...toolchainv1alpha1.Condition) error {
 	hostAwait := NewHostAwaitility(awaitility)
 	memberAwait := NewMemberAwaitility(awaitility)
-	if err := hostAwait.waitForMasterUserRecord(mur.Name); err != nil {
+	if err := hostAwait.WaitForMasterUserRecord(mur.Name); err != nil {
 		return err
 	}
-	if err := memberAwait.waitForUserAccount(mur.Name, mur.Spec.UserAccounts[0].Spec); err != nil {
+	murUserAccount := mur.Spec.UserAccounts[0]
+	if err := memberAwait.WaitForUserAccount(mur.Name, murUserAccount.Spec, uaStatusConds...); err != nil {
 		return err
 	}
-	if err := hostAwait.waitForMurConditions(mur.Name, murCond); err != nil {
+	userAccount := memberAwait.GetUserAccount(mur.Name)
+	uaStatus := toolchainv1alpha1.UserAccountStatusEmbedded{
+		TargetCluster:     murUserAccount.TargetCluster,
+		UserAccountStatus: userAccount.Status,
+	}
+	if err := hostAwait.WaitForMurConditions(mur.Name,
+		UntilHasStatusCondition(toBeProvisioned()),
+		UntilHasUserAccountStatus(uaStatus)); err != nil {
 		return err
 	}
 	return nil
+}
+
+func coolStatus() toolchainv1alpha1.Condition {
+	return toolchainv1alpha1.Condition{
+		Type:    toolchainv1alpha1.ConditionType("CoolType"),
+		Status:  v1.ConditionTrue,
+		Reason:  "EverythingIsGood",
+		Message: "because our SaaS is cool",
+	}
 }
 
 func toBeProvisioned() toolchainv1alpha1.Condition {
@@ -58,14 +118,6 @@ func toBeProvisioned() toolchainv1alpha1.Condition {
 		Type:   toolchainv1alpha1.ConditionReady,
 		Status: v1.ConditionTrue,
 		Reason: "Provisioned",
-	}
-}
-
-func toBeProvisioning() toolchainv1alpha1.Condition {
-	return toolchainv1alpha1.Condition{
-		Type:   toolchainv1alpha1.ConditionReady,
-		Status: v1.ConditionFalse,
-		Reason: "Provisioning",
 	}
 }
 
--- test/e2e/wait_util.go
+++ test/e2e/wait_util.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
+	"github.com/stretchr/testify/require"
 	"reflect"
 
 	"github.com/codeready-toolchain/toolchain-common/pkg/test/e2e"
@@ -28,7 +29,7 @@ func NewMemberAwaitility(awaitility *e2e.Awaitility) *MemberAwaitility {
 	return &MemberAwaitility{SingleAwaitility: awaitility.Member()}
 }
 
-func (a *HostAwaitility) waitForMasterUserRecord(name string) error {
+func (a *HostAwaitility) WaitForMasterUserRecord(name string) error {
 	return wait.Poll(e2e.RetryInterval, e2e.Timeout, func() (done bool, err error) {
 		mur := &toolchainv1alpha1.MasterUserRecord{}
 		if err := a.Client.Get(context.TODO(), types.NamespacedName{Namespace: a.Ns, Name: name}, mur); err != nil {
@@ -43,7 +44,14 @@ func (a *HostAwaitility) waitForMasterUserRecord(name string) error {
 	})
 }
 
-func (a *HostAwaitility) waitForMurConditions(name string, conditions ...toolchainv1alpha1.Condition) error {
+func (a *HostAwaitility) GetMasterUserRecord(name string) *toolchainv1alpha1.MasterUserRecord {
+	mur := &toolchainv1alpha1.MasterUserRecord{}
+	err := a.Client.Get(context.TODO(), types.NamespacedName{Namespace: a.Ns, Name: name}, mur)
+	require.NoError(a.T, err)
+	return mur
+}
+
+func (a *HostAwaitility) WaitForMurConditions(name string, waitCond ...MurWaitCondition) error {
 	return wait.Poll(e2e.RetryInterval, e2e.Timeout, func() (done bool, err error) {
 		mur := &toolchainv1alpha1.MasterUserRecord{}
 		if err := a.Client.Get(context.TODO(), types.NamespacedName{Namespace: a.Ns, Name: name}, mur); err != nil {
@@ -53,16 +61,66 @@ func (a *HostAwaitility) waitForMurConditions(name string, conditions ...toolcha
 			}
 			return false, err
 		}
-		if test.ConditionsMatch(mur.Status.Conditions, conditions...) {
-			a.T.Log("conditions match")
-			return true, nil
+		for _, isMatched := range waitCond {
+			if !isMatched(a, mur) {
+				return false, nil
+			}
 		}
-		a.T.Logf("waiting for correct condition of MasterUserRecord '%s", name)
-		return false, nil
+		return true, nil
 	})
 }
 
-func (a *MemberAwaitility) waitForUserAccount(name string, expSpec toolchainv1alpha1.UserAccountSpec) error {
+type MurWaitCondition func(a *HostAwaitility, mur *toolchainv1alpha1.MasterUserRecord) bool
+
+func UntilHasStatusCondition(conditions ...toolchainv1alpha1.Condition) MurWaitCondition {
+	return func(a *HostAwaitility, mur *toolchainv1alpha1.MasterUserRecord) bool {
+		if test.ConditionsMatch(mur.Status.Conditions, conditions...) {
+			a.T.Logf("status conditions match in MasterUserRecord '%s`", mur.Name)
+			return true
+		}
+		a.T.Logf("waiting for correct status condition of MasterUserRecord '%s`", mur.Name)
+		return false
+	}
+}
+
+func UntilHasUserAccountStatus(expUaStatuses ...toolchainv1alpha1.UserAccountStatusEmbedded) MurWaitCondition {
+	return func(a *HostAwaitility, mur *toolchainv1alpha1.MasterUserRecord) bool {
+		if len(mur.Status.UserAccounts) != len(expUaStatuses) {
+			a.T.Logf("waiting for corrent number of UserAccount statuses in MasterUserRecord '%s`", mur.Name)
+			return false
+		}
+		for _, expUaStatus := range expUaStatuses {
+			expUaStatus.SyncIndex = getUaSpecSyncIndex(mur, expUaStatus.TargetCluster)
+			if !containsUserAccountStatus(mur.Status.UserAccounts, expUaStatus) {
+				a.T.Logf("waiting for UserAccount status to be present in MasterUserRecord '%s`", mur.Name)
+				return false
+			}
+
+		}
+		a.T.Logf("all UserAccount statuses are present in MasterUserRecord '%s`", mur.Name)
+		return true
+	}
+}
+
+func getUaSpecSyncIndex(mur *toolchainv1alpha1.MasterUserRecord, targetCluster string) string {
+	for _, ua := range mur.Spec.UserAccounts {
+		if ua.TargetCluster == targetCluster {
+			return ua.SyncIndex
+		}
+	}
+	return ""
+}
+
+func containsUserAccountStatus(uaStatuses []toolchainv1alpha1.UserAccountStatusEmbedded, uaStatus toolchainv1alpha1.UserAccountStatusEmbedded) bool {
+	for _, status := range uaStatuses {
+		if reflect.DeepEqual(uaStatus, status) {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *MemberAwaitility) WaitForUserAccount(name string, expSpec toolchainv1alpha1.UserAccountSpec, conditions ...toolchainv1alpha1.Condition) error {
 	return wait.Poll(e2e.RetryInterval, e2e.Timeout, func() (done bool, err error) {
 		ua := &toolchainv1alpha1.UserAccount{}
 		if err := a.Client.Get(context.TODO(), types.NamespacedName{Namespace: a.Ns, Name: name}, ua); err != nil {
@@ -72,10 +130,19 @@ func (a *MemberAwaitility) waitForUserAccount(name string, expSpec toolchainv1al
 			}
 			return false, err
 		}
-		if reflect.DeepEqual(ua.Spec, expSpec) {
-			a.T.Logf("found UserAccount '%s' with expected spec", name)
+		if reflect.DeepEqual(ua.Spec, expSpec) &&
+			test.ConditionsMatch(ua.Status.Conditions, conditions...) {
+			a.T.Logf("found UserAccount '%s' with expected spec and status condition", name)
 			return true, nil
 		}
+		a.T.Logf("waiting for UserAccount '%s' with expected spec and status condition", name)
 		return false, nil
 	})
+}
+
+func (a *MemberAwaitility) GetUserAccount(name string) *toolchainv1alpha1.UserAccount {
+	ua := &toolchainv1alpha1.UserAccount{}
+	err := a.Client.Get(context.TODO(), types.NamespacedName{Namespace: a.Ns, Name: name}, ua)
+	require.NoError(a.T, err)
+	return ua
 }
