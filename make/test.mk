7447a7187a9c7fa56f04e9cda0c2f55ba3d9922c
Matous Jobanek <mjobanek@redhat.com>
2019-09-11 11:55:18 +0200

    chore(CRT-222): move e2e tests to toolchain-e2e (#58)

--- make/test.mk
+++ make/test.mk
@@ -64,91 +64,57 @@ BASE_COMMIT := $(shell echo $$CLONEREFS_OPTIONS | jq '.refs[0].base_sha')
 PR_COMMIT := $(shell echo $$CLONEREFS_OPTIONS | jq '.refs[0].pulls[0].sha')
 PULL_NUMBER := $(shell echo $$CLONEREFS_OPTIONS | jq '.refs[0].pulls[0].number')
 
-MEMBER_NS := member-operator-$(shell date +'%s')
-HOST_NS := host-operator-$(shell date +'%s')
-
 ###########################################################
 #
 # End-to-end Tests
 #
 ###########################################################
 
-.PHONY: test-e2e-keep-namespaces
-test-e2e-keep-namespaces: deploy-member e2e-setup setup-kubefed e2e-run
+AUTHOR_LINK := $(shell jq -r '.refs[0].pulls[0].author_link' <<< $${CLONEREFS_OPTIONS} | tr -d '[:space:]')
+PULL_SHA := $(shell jq -r '.refs[0].pulls[0].sha' <<< $${CLONEREFS_OPTIONS} | tr -d '[:space:]')
+E2E_REPO_PATH := ""
+
+.PHONY: test-e2e-local
+test-e2e-local:
+	$(MAKE) test-e2e E2E_REPO_PATH=../toolchain-e2e
 
 .PHONY: test-e2e
-test-e2e: test-e2e-keep-namespaces e2e-cleanup
+test-e2e: get-e2e-repo
+	# run the e2e test via toolchain-e2e repo
+	$(MAKE) -C ${E2E_REPO_PATH} test-e2e HOST_REPO_PATH=${PWD}
 
-.PHONY: e2e-run
-e2e-run:
-	oc get kubefedcluster -n $(HOST_NS)
-	oc get kubefedcluster -n $(MEMBER_NS)
-	MEMBER_NS=${MEMBER_NS} operator-sdk test local ./test/e2e --no-setup --namespace $(HOST_NS) --verbose --go-test-flags "-timeout=15m" || \
-	($(MAKE) print-logs HOST_NS=${HOST_NS} MEMBER_NS=${MEMBER_NS} && exit 1)
-
-.PHONY: print-logs
-print-logs:
-	@echo "=====================================================================================" &
-	@echo "================================ Host cluster logs =================================="
-	@echo "====================================================================================="
-	@oc logs deployment.apps/host-operator --namespace $(HOST_NS)
-	@echo "====================================================================================="
-	@echo "================================ Member cluster logs ================================"
-	@echo "====================================================================================="
-	@oc logs deployment.apps/member-operator --namespace $(MEMBER_NS)
-	@echo "====================================================================================="
-
-.PHONY: e2e-setup
-e2e-setup: is-minishift
-	oc new-project $(HOST_NS) --display-name e2e-tests
-	oc apply -f ./deploy/service_account.yaml
-	oc apply -f ./deploy/role.yaml
-	oc apply -f ./deploy/role_binding.yaml
-	oc apply -f ./deploy/cluster_role.yaml
-	sed -e 's|REPLACE_NAMESPACE|${HOST_NS}|g' ./deploy/cluster_role_binding.yaml  | oc apply -f -
-	oc apply -f deploy/crds
-	sed -e 's|REPLACE_IMAGE|${IMAGE_NAME}|g' ./deploy/operator.yaml  | oc apply -f -
-
-.PHONY: is-minishift
-is-minishift:
-ifeq ($(OPENSHIFT_BUILD_NAMESPACE),)
-	$(info logging as system:admin")
-	$(shell echo "oc login -u system:admin")
-	$(eval IMAGE_NAME := docker.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT})
-	$(shell echo "make docker-image")
-else
-	$(eval IMAGE_NAME := registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:host-operator)
+.PHONY: get-e2e-repo
+get-e2e-repo:
+ifeq ($(E2E_REPO_PATH),"")
+	# set e2e repo path to tmp directory
+	$(eval E2E_REPO_PATH = /tmp/toolchain-e2e)
+	# delete to have clear environment
+	rm -rf ${E2E_REPO_PATH}
+	# clone
+	git clone https://github.com/codeready-toolchain/toolchain-e2e.git --depth 1 ${E2E_REPO_PATH}
+    ifneq ($(CLONEREFS_OPTIONS),)
+		@echo "using author link ${AUTHOR_LINK}"
+		@echo "using pull sha ${PULL_SHA}"
+		# get branch ref of the fork the PR was created from
+		$(eval BRANCH_REF := $(shell curl ${AUTHOR_LINK}/host-operator.git/info/refs?service=git-upload-pack --output - /dev/null 2>&1 | grep -a ${PULL_SHA} | awk '{print $$2}'))
+		@echo "detected branch ref ${BRANCH_REF}"
+		# check if a branch with the same ref exists in the user's fork of toolchain-e2e repo
+		$(eval REMOTE_E2E_BRANCH := $(shell curl ${AUTHOR_LINK}/toolchain-e2e.git/info/refs?service=git-upload-pack --output - /dev/null 2>&1 | grep -a ${BRANCH_REF} | awk '{print $$2}'))
+		@echo "branch ref of the user's fork: \"${REMOTE_E2E_BRANCH}\" - if empty then not found"
+		# check if the branch with the same name exists, if so then merge it with master and use the merge branch, if not then use master
+		if [[ -n "${REMOTE_E2E_BRANCH}" ]]; then \
+			# retrieve the branch name \
+			BRANCH_NAME=`echo ${BRANCH_REF} | awk -F'/' '{print $$3}'`; \
+			# add the user's fork as remote repo \
+			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} remote add external ${AUTHOR_LINK}/toolchain-e2e.git; \
+			# fetch the branch \
+			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} fetch external ${BRANCH_REF}; \
+			# merge the branch with master \
+			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} merge FETCH_HEAD; \
+		fi;
+    endif
 endif
-
-.PHONY: setup-kubefed
-setup-kubefed:
-	curl -sSL https://raw.githubusercontent.com/codeready-toolchain/toolchain-common/master/scripts/add-cluster.sh | bash -s -- -t member -mn $(MEMBER_NS) -hn $(HOST_NS) -s
-	curl -sSL https://raw.githubusercontent.com/codeready-toolchain/toolchain-common/master/scripts/add-cluster.sh | bash -s -- -t host -mn $(MEMBER_NS) -hn $(HOST_NS) -s
-
-.PHONY: e2e-cleanup
-e2e-cleanup:
-	oc delete project ${MEMBER_NS} ${HOST_NS} --wait=false || true
 
 .PHONY: clean-e2e-namespaces
 clean-e2e-namespaces:
-	$(Q)-oc get projects --output=name | grep -E "(member|host)\-operator\-[0-9]+" | xargs oc delete
-
-###########################################################
-#
-# Deploying Member Operator in Openshift CI Environment
-#
-###########################################################
-
-.PHONY: deploy-member
-deploy-member:
-	rm -rf /tmp/member-operator
-	# cloning here as don't want to maintain it for every single change in deploy directory of member-operator
-	git clone https://github.com/codeready-toolchain/member-operator.git --depth 1 /tmp/member-operator
-	oc new-project $(MEMBER_NS)
-	oc apply -f /tmp/member-operator/deploy/service_account.yaml
-	oc apply -f /tmp/member-operator/deploy/role.yaml
-	oc apply -f /tmp/member-operator/deploy/role_binding.yaml
-	oc apply -f /tmp/member-operator/deploy/cluster_role.yaml
-	cat /tmp/member-operator/deploy/cluster_role_binding.yaml | sed s/\REPLACE_NAMESPACE/$(MEMBER_NS)/ | oc apply -f -
-	oc apply -f /tmp/member-operator/deploy/crds
-	sed -e 's|REPLACE_IMAGE|registry.svc.ci.openshift.org/codeready-toolchain/member-operator-v0.1:member-operator|g' /tmp/member-operator/deploy/operator.yaml  | oc apply -f -
+	$(Q)-oc get projects --output=name | grep -E "(member|host)\-operator\-[0-9]+|toolchain\-e2e\-[0-9]+" | xargs oc delete
