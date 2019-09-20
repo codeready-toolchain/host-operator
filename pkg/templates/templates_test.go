960122f68f1853de0c08a7a4d16b885071294577
Xavier Coulon <xcoulon@redhat.com>
2019-09-20 18:15:46 +0200

    verify the commits
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/templates/templates_test.go
+++ pkg/templates/templates_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/yaml.v2"
 )
 
@@ -37,11 +38,29 @@ func TestTemplates(t *testing.T) {
 		metadata := make(map[string]interface{})
 		err = yaml.Unmarshal(asset, &metadata)
 		require.NoError(t, err)
+		repo, err := git.PlainOpen("../..")
+		require.NoError(t, err)
+		ref, err := repo.Head()
+		require.NoError(t, err)
 
 		// when/then
 		for _, tier := range tiers {
 			for _, nsType := range nsTypes {
+				filename := fmt.Sprintf("deploy/nstemplatetiers/%s-%s.yaml", tier, nsType)
+				commit, found := metadata[fmt.Sprintf("%s-%s", tier, nsType)]
+				require.True(t, found)
 				assert.Contains(t, metadata, fmt.Sprintf("%s-%s", tier, nsType))
+
+				// verify that the commit in the metadata file matches the
+				// actually commit in the repository
+				cIter, err := repo.Log(&git.LogOptions{
+					From:     ref.Hash(),
+					FileName: &filename,
+				})
+				require.NoError(t, err)
+				c, err := cIter.Next()
+				require.NoError(t, err)
+				assert.Equal(t, c.ID().String()[0:7], commit)
 			}
 		}
 	})
