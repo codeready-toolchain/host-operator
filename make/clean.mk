c6c6ea7d6799b12f8ea46f3dc4608de0c0a8b24f
Xavier Coulon <xcoulon@redhat.com>
2019-09-30 08:57:39 +0200

    remove redirects to /dev/null
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/clean.mk
+++ make/clean.mk
@@ -2,5 +2,5 @@
 clean:
 	$(Q)-rm -rf ${V_FLAG} $(OUT_DIR) ./vendor
 	$(Q)go clean ${X_FLAG} ./...
-	$(Q)-rm pkg/templates/nstemplatetiers/nstemplatetier_assets.go > /dev/null
-	$(Q)-rm deploy/templates/nstemplatetiers/metadata.yaml > /dev/null
+	$(Q)-rm pkg/templates/nstemplatetiers/nstemplatetier_assets.go 
+	$(Q)-rm deploy/templates/nstemplatetiers/metadata.yaml
