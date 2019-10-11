98141a63299e7df41f9727913dfba5a9914cf7aa
Matous Jobanek <mjobanek@redhat.com>
2019-10-11 20:46:46 +0200

    fix: create KubeFedCluster CRD before client and cache are started

--- pkg/controller/kubefedcluster.go
+++ pkg/controller/kubefedcluster.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
 	"github.com/codeready-toolchain/toolchain-common/pkg/controller"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"k8s.io/klog"
@@ -11,10 +10,6 @@ import (
 )
 
 func StartKubeFedClusterControllers(mgr manager.Manager, stopChan <-chan struct{}) error {
-	if err := cluster.EnsureKubeFedClusterCrd(mgr.GetScheme(), mgr.GetClient()); err != nil {
-		return err
-	}
-
 	if err := startHealthCheckController(mgr, stopChan); err != nil {
 		return err
 	}
