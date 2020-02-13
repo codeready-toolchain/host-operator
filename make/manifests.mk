69415084ab79ad89714b3ab378bef740ef648c50
Matous Jobanek <mjobanek@redhat.com>
2020-02-13 13:35:30 +0000

    chore: add generate-olm-files make target

--- make/manifests.mk
+++ make/manifests.mk
@@ -1,14 +1,28 @@
 
 PATH_TO_PUSH_NIGHTLY_FILE=scripts/push-to-quay-nightly.sh
+PATH_TO_OLM_GENERATE_FILE=scripts/olm-catalog-generate.sh
 
 .PHONY: push-to-quay-nightly
 ## Creates a new version of CSV and pushes it to quay
 push-to-quay-nightly:
 	$(eval PUSH_PARAMS = -pr ../host-operator/ -er https://github.com/codeready-toolchain/registration-service/ -qn ${QUAY_NAMESPACE})
 ifneq ("$(wildcard ../api/$(PATH_TO_PUSH_NIGHTLY_FILE))","")
-	@echo "creating release manifest in ./manifest/ directory using script from local api repo..."
+	@echo "pushing to quay in nightly channel using script from local api repo..."
 	../api/${PATH_TO_PUSH_NIGHTLY_FILE} ${PUSH_PARAMS}
 else
-	@echo "creating release manifest in ./manifest/ directory using script from GH api repo (using latest version in master)..."
+	@echo "pushing to quay in nightly channel using script from GH api repo (using latest version in master)..."
 	curl -sSL https://raw.githubusercontent.com/codeready-toolchain/api/master/${PATH_TO_PUSH_NIGHTLY_FILE} | bash -s -- ${PUSH_PARAMS}
+endif
+
+
+.PHONY: generate-olm-files
+## Regenerates base template CSV and hack files
+generate-olm-files:
+	$(eval GENERATE_PARAMS = -pr ../host-operator/)
+ifneq ("$(wildcard ../api/$(PATH_TO_OLM_GENERATE_FILE))","")
+	@echo "generating OLM files using script from local api repo..."
+	../api/${PATH_TO_OLM_GENERATE_FILE} ${GENERATE_PARAMS}
+else
+	@echo "generating OLM files using script from GH api repo (using latest version in master)..."
+	curl -sSL https://raw.githubusercontent.com/codeready-toolchain/api/master/${PATH_TO_OLM_GENERATE_FILE} | bash -s -- ${GENERATE_PARAMS}
 endif
