28eaba6064376ab4ef7fedf73d90379018881fb6
Alexey Kazakov <alkazako@redhat.com>
2020-03-06 11:04:51 -0800

    Move team tier to the proper dir

--- make/clean.mk
+++ make/clean.mk
@@ -2,6 +2,8 @@
 clean:
 	$(Q)-rm -rf ${V_FLAG} $(OUT_DIR) ./vendor
 	$(Q)go clean ${X_FLAG} ./...
-	$(Q)-rm deploy/templates/nstemplatetiers/metadata.yaml
-	$(Q)-rm pkg/templates/nstemplatetiers/nstemplatetier_assets.go 2>/dev/null
-	$(Q)-rm test/templates/nstemplatetiers/nstemplatetier_assets.go 2>/dev/null
+	$(Q)-rm deploy/templates/nstemplatetiers/metadata.yaml 2>/dev/null || true
+	$(Q)-rm deploy/templates/nstemplatetiers/namespaces/metadata.yaml 2>/dev/null || true
+	$(Q)-rm deploy/templates/nstemplatetiers/clusterresources/metadata.yaml 2>/dev/null || true
+	$(Q)-rm pkg/templates/nstemplatetiers/nstemplatetier_assets.go 2>/dev/null || true
+	$(Q)-rm test/templates/nstemplatetiers/nstemplatetier_assets.go 2>/dev/null || true
