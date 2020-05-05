eae2f7888550c613e9e4b9c88261051b3e9d0eea
Xavier Coulon <xcoulon@redhat.com>
2020-05-05 09:04:47 +0200

    Update pkg/templates/nstemplatetiers/nstemplatetier_generator_test.go
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

--- pkg/templates/nstemplatetiers/nstemplatetier_generator_test.go
+++ pkg/templates/nstemplatetiers/nstemplatetier_generator_test.go
@@ -232,7 +232,7 @@ func TestCreateOrUpdateResources(t *testing.T) {
 
 			// then
 			require.NoError(t, err)
-			// verify that all TemplateTier CRs were updated
+			// verify that all TierTemplate CRs for the new revisions were created
 			tierTmpls := toolchainv1alpha1.TierTemplateList{}
 			err = clt.List(context.TODO(), &tierTmpls, client.InNamespace(namespace))
 			require.NoError(t, err)
