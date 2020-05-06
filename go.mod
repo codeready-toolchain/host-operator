529d555660eb805692e020ca395229b21033cbd0
Xavier Coulon <xcoulon@redhat.com>
2020-05-06 18:08:59 +0200

    remove unused funcs, update assertions
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- go.mod
+++ go.mod
@@ -3,34 +3,43 @@ module github.com/codeready-toolchain/host-operator
 require (
 	cloud.google.com/go v0.46.3 // indirect
 	github.com/Azure/go-autorest/autorest/adal v0.6.0 // indirect
-	github.com/codeready-toolchain/api v0.0.0-20200427134136-024db3a3f3bd
-	github.com/codeready-toolchain/toolchain-common v0.0.0-20200427080755-4d31a88fd82c
+	github.com/codeready-toolchain/api v0.0.0-20200506130228-a821d9c227f1
+	github.com/codeready-toolchain/toolchain-common v0.0.0-20200504150510-6252228f3527
+	github.com/fatih/color v1.9.0 // indirect
 	github.com/go-bindata/go-bindata v3.1.2+incompatible
 	github.com/go-logr/logr v0.1.0
 	github.com/go-openapi/swag v0.19.9 // indirect
+	github.com/gobuffalo/flect v0.2.1 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/gophercloud/gophercloud v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/openshift/api v3.9.1-0.20190924102528-32369d4db2ad+incompatible
 	github.com/operator-framework/operator-sdk v0.16.0
 	github.com/pkg/errors v0.9.1
 	github.com/redhat-cop/operator-utils v0.0.0-20190827162636-51e6b0c32776
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cast v1.3.0
+	github.com/spf13/cobra v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.1
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
-	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
+	golang.org/x/net v0.0.0-20200505041828-1ed23360d12c // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/tools v0.0.0-20200505023115-26f46d2f7ef8 // indirect
 	gopkg.in/h2non/gock.v1 v1.0.14
 	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v3 v3.0.0-20200504163728-5308cda29e3d // indirect
 	k8s.io/api v0.18.2
 	k8s.io/apiextensions-apiserver v0.18.2
 	k8s.io/apimachinery v0.18.2
 	k8s.io/client-go v12.0.0+incompatible
+	k8s.io/code-generator v0.18.2 // indirect
+	k8s.io/gengo v0.0.0-20200428234225-8167cfdcfc14 // indirect
 	k8s.io/klog v1.0.0
-	k8s.io/kube-openapi v0.0.0-20200413232311-afe0b5e9f729 // indirect
+	k8s.io/kube-openapi v0.0.0-20200427153329-656914f816f9 // indirect
 	k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
 	sigs.k8s.io/controller-runtime v0.5.0
+	sigs.k8s.io/controller-tools v0.3.0 // indirect
 	sigs.k8s.io/kubefed v0.1.0-rc6.0.20200224204536-6207193c49f7
 )
 
--- pkg/templates/nstemplatetiers/nstemplatetier_generator_test.go
+++ pkg/templates/nstemplatetiers/nstemplatetier_generator_test.go
@@ -15,7 +15,6 @@ import (
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -31,52 +30,6 @@ func TestCreateOrUpdateResources(t *testing.T) {
 	err := apis.AddToScheme(s)
 	require.NoError(t, err)
 	logf.SetLogger(zap.Logger(true))
-	mockCreate := func(clt *testsupport.FakeClient) func(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
-		return func(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
-			var objMeta *metav1.ObjectMeta
-			switch obj := obj.(type) {
-			case *toolchainv1alpha1.NSTemplateTier:
-				objMeta = &obj.ObjectMeta
-			case *toolchainv1alpha1.TierTemplate:
-				objMeta = &obj.ObjectMeta
-			default:
-				return errors.Errorf("did not expect to create a resource of type %T", obj)
-			}
-			if objMeta.Generation != 0 {
-				return errors.Errorf("'generation' field will be specified by the server during object creation: %v", objMeta.Generation)
-			}
-			objMeta.Generation = 1
-			objMeta.ResourceVersion = "foo" // set by the server
-			err := clt.Client.Create(ctx, obj)
-			if err != nil && apierrors.IsAlreadyExists(err) {
-				// prevent the fake client to return the actual ResourceVersion,
-				// as it seems like the real client/server don't not do it in the e2e tests
-				// see client.MockUpdate just underneath for the use-case
-				objMeta.ResourceVersion = ""
-			}
-			return err
-		}
-	}
-
-	// check the 'generation' when updating the object, and increment its value
-	mockUpdate := func(clt *testsupport.FakeClient) func(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
-		return func(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
-			if obj, ok := obj.(*toolchainv1alpha1.NSTemplateTier); ok {
-				if obj.ObjectMeta.ResourceVersion != "foo" {
-					// here, we expect that the caller will have set the ResourceVersion based on the existing object
-					return errors.Errorf("'ResourceVersion ' field must be specified during object update")
-				}
-				// increment even if the object did not change
-				existing := toolchainv1alpha1.NSTemplateTier{}
-				if err := clt.Get(ctx, types.NamespacedName{Namespace: obj.GetNamespace(), Name: obj.GetName()}, &existing); err != nil {
-					return err
-				}
-				obj.SetGeneration(existing.GetGeneration() + 1)
-				return clt.Client.Update(ctx, obj)
-			}
-			return errors.Errorf("did not expect to update a resource of type %T", obj)
-		}
-	}
 
 	assets := nstemplatetiers.NewAssets(testnstemplatetiers.AssetNames, testnstemplatetiers.Asset)
 
@@ -86,8 +39,6 @@ func TestCreateOrUpdateResources(t *testing.T) {
 			// given
 			namespace := "host-operator" + uuid.NewV4().String()[:7]
 			clt := testsupport.NewFakeClient(t)
-			clt.MockCreate = mockCreate(clt)
-			clt.MockUpdate = mockUpdate(clt)
 			// verify that no NSTemplateTier resources exist prior to creation
 			nsTmplTiers := toolchainv1alpha1.NSTemplateTierList{}
 			err = clt.List(context.TODO(), &nsTmplTiers, client.InNamespace(namespace))
@@ -157,8 +108,6 @@ func TestCreateOrUpdateResources(t *testing.T) {
 			// given
 			namespace := "host-operator" + uuid.NewV4().String()[:7]
 			clt := testsupport.NewFakeClient(t)
-			clt.MockCreate = mockCreate(clt)
-			clt.MockUpdate = mockUpdate(clt)
 
 			// when
 			err := nstemplatetiers.CreateOrUpdateResources(s, clt, namespace, assets)
@@ -178,13 +127,13 @@ func TestCreateOrUpdateResources(t *testing.T) {
 				assert.Equal(t, int64(1), tierTmpl.ObjectMeta.Generation) // unchanged
 			}
 
-			// verify that the 4 NStemplateTier CRs were updated
+			// verify that the 4 NStemplateTier CRs were NOT updated since assets did not change
 			for _, tierName := range []string{"advanced", "basic", "team", "nocluster"} {
 				tier := toolchainv1alpha1.NSTemplateTier{}
 				err = clt.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: tierName}, &tier)
 				require.NoError(t, err)
-				// verify that the generation was increased
-				assert.Equal(t, int64(2), tier.ObjectMeta.Generation)
+				// verify that the generation was not changed 
+				assert.Equal(t, int64(1), tier.ObjectMeta.Generation)
 				// verify the new namespace revisions
 				for _, ns := range tier.Spec.Namespaces {
 					assert.Equal(t, nstemplatetiers.ExpectedRevisions[tierName][ns.Type], ns.Revision)
@@ -198,8 +147,6 @@ func TestCreateOrUpdateResources(t *testing.T) {
 			// given
 			namespace := "host-operator" + uuid.NewV4().String()[:7]
 			clt := testsupport.NewFakeClient(t)
-			clt.MockCreate = mockCreate(clt)
-			clt.MockUpdate = mockUpdate(clt)
 
 			// when
 			err := nstemplatetiers.CreateOrUpdateResources(s, clt, namespace, assets)
