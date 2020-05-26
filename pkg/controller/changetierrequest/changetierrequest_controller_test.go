8916c92bc1e7e6d1095e7584af91f2cab44e76aa
Xavier Coulon <xcoulon@redhat.com>
2020-05-26 16:34:46 +0200

    remove unused import
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/controller/changetierrequest/changetierrequest_controller_test.go
+++ pkg/controller/changetierrequest/changetierrequest_controller_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-	toolchainv1alpha1 "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/host-operator/pkg/apis"
 	"github.com/codeready-toolchain/host-operator/pkg/configuration"
 	"github.com/codeready-toolchain/host-operator/pkg/templates/nstemplatetiers"
