9eb0570cc8ef429bb540c3fb1b7e5c670cb7de3d
Tina Kurian <tkurian@redhat.com>
2020-06-02 13:43:39 -0400

    deprecating nstemplateset fields  (#212)
    * generated
    
    * re-adding tierName
    
    * updating api and common

--- pkg/controller/usersignup/masteruserrecord_test.go
+++ pkg/controller/usersignup/masteruserrecord_test.go
@@ -70,7 +70,7 @@ func TestMigrateMurIfNecessary(t *testing.T) {
 		mur := newMasterUserRecord(nSTemplateTier, "johny", operatorNamespace, test.MemberClusterName, "123456789")
 
 		// when
-		changed, changedMur := migrateMurIfNecessary(mur, nSTemplateTier)
+		changed, changedMur := migrateOrFixMurIfNecessary(mur, nSTemplateTier)
 
 		// then
 		assert.False(t, changed)
@@ -84,7 +84,7 @@ func TestMigrateMurIfNecessary(t *testing.T) {
 		mur.Spec.UserAccounts[0].Spec.NSLimit = ""
 
 		// when
-		changed, changedMur := migrateMurIfNecessary(mur, nSTemplateTier)
+		changed, changedMur := migrateOrFixMurIfNecessary(mur, nSTemplateTier)
 
 		// then
 		assert.True(t, changed)
@@ -98,41 +98,7 @@ func TestMigrateMurIfNecessary(t *testing.T) {
 		mur.Spec.UserAccounts[0].Spec.NSTemplateSet = v1alpha1.NSTemplateSetSpec{}
 
 		// when
-		changed, changedMur := migrateMurIfNecessary(mur, nSTemplateTier)
-
-		// then
-		assert.True(t, changed)
-		assert.Equal(t, newExpectedMur(), *changedMur)
-	})
-
-	t.Run("when NSTemplateSet is missing templateRefs", func(t *testing.T) {
-		// given
-		nSTemplateTier := newNsTemplateTier("advanced", "654321b", "dev", "stage", "extra")
-		mur := newMasterUserRecord(nSTemplateTier, "johny", operatorNamespace, test.MemberClusterName, "123456789")
-		mur.Spec.UserAccounts[0].Spec.NSTemplateSet.Namespaces = []v1alpha1.NSTemplateSetNamespace{
-			{
-				Type:        "dev",
-				Revision:    "123abc1",
-				TemplateRef: "",
-			},
-			{
-				Type:        "stage",
-				Revision:    "123abc2",
-				TemplateRef: "",
-			},
-			{
-				Type:        "extra",
-				Revision:    "123abc3",
-				TemplateRef: "",
-			},
-		}
-		mur.Spec.UserAccounts[0].Spec.NSTemplateSet.ClusterResources = &v1alpha1.NSTemplateSetClusterResources{
-			Revision:    "654321b",
-			TemplateRef: "",
-		}
-
-		// when
-		changed, changedMur := migrateMurIfNecessary(mur, nSTemplateTier)
+		changed, changedMur := migrateOrFixMurIfNecessary(mur, nSTemplateTier)
 
 		// then
 		assert.True(t, changed)
@@ -143,12 +109,11 @@ func TestMigrateMurIfNecessary(t *testing.T) {
 		// given
 		nSTemplateTier := newNsTemplateTier("advanced", "654321b", "dev", "stage", "extra")
 		mur := newMasterUserRecord(nSTemplateTier, "johny", operatorNamespace, test.MemberClusterName, "123456789")
-		mur.Spec.UserAccounts[0].Spec.NSTemplateSet.Namespaces[0].Type = "cicd"
 		mur.Spec.UserAccounts[0].Spec.NSTemplateSet.Namespaces[0].TemplateRef = "advanced-cicd-123abc1"
 		providedMur := mur.DeepCopy()
 
 		// when
-		changed, changedMur := migrateMurIfNecessary(mur, nSTemplateTier)
+		changed, changedMur := migrateOrFixMurIfNecessary(mur, nSTemplateTier)
 
 		// then
 		assert.False(t, changed)
