dd989b793f8e9ba5f6f66d5df546c650318e17f8
sbryzak <sbryzak@gmail.com>
2021-03-05 14:25:00 +1000

    CRT-895 add another test
    Signed-off-by: sbryzak <sbryzak@gmail.com>

--- pkg/controller/usercleanup/user_cleanup_controller_test.go
+++ pkg/controller/usercleanup/user_cleanup_controller_test.go
@@ -1,6 +1,7 @@
 package usercleanup
 
 import (
+	"context"
 	"github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 	"github.com/codeready-toolchain/host-operator/pkg/apis"
 	"github.com/codeready-toolchain/host-operator/pkg/configuration"
@@ -9,16 +10,18 @@ import (
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"testing"
+	"time"
 
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
 func TestUserCleanup(t *testing.T) {
-	t.Run("test that user cleanup doesn't delete an existing non-deactivated UserSignup", func(t *testing.T) {
+	t.Run("test that user cleanup doesn't delete an active UserSignup", func(t *testing.T) {
 		userSignup := &v1alpha1.UserSignup{
 			ObjectMeta: test2.NewUserSignupObjectMeta("", "abigail.thompson@redhat.com"),
 			Spec: v1alpha1.UserSignupSpec{
@@ -48,6 +51,52 @@ func TestUserCleanup(t *testing.T) {
 
 		_, err := r.Reconcile(req)
 		require.NoError(t, err)
+
+		// Confirm the UserSignup still exists
+		key := test.NamespacedName(test.HostOperatorNs, userSignup.Name)
+		require.NoError(t, r.client.Get(context.Background(), key, userSignup))
+		require.NotNil(t, userSignup)
+	})
+
+	t.Run("test that user cleanup doesn't delete a recently deactivated UserSignup", func(t *testing.T) {
+
+		userSignup := &v1alpha1.UserSignup{
+			ObjectMeta: test2.NewUserSignupObjectMeta("", "brian.anderson@redhat.com"),
+			Spec: v1alpha1.UserSignupSpec{
+				UserID:      "User-93321",
+				Username:    "brian.anderson@redhat.com",
+				Approved:    true,
+				Deactivated: true,
+			},
+			Status: v1alpha1.UserSignupStatus{
+				Conditions: []v1alpha1.Condition{
+					{
+						Type:               v1alpha1.UserSignupComplete,
+						Status:             v1.ConditionTrue,
+						Reason:             v1alpha1.UserSignupUserDeactivatedReason,
+						LastTransitionTime: metav1.Time{Time: time.Now()},
+					},
+					{
+						Type:   v1alpha1.UserSignupApproved,
+						Status: v1.ConditionTrue,
+						Reason: "ApprovedAutomatically",
+					},
+				},
+				CompliantUsername: "brian.anderson",
+			},
+		}
+		userSignup.Labels["toolchain.dev.openshift.com/approved"] = "true"
+		userSignup.Labels[v1alpha1.UserSignupStateLabelKey] = "approved"
+
+		r, req, _ := prepareReconcile(t, userSignup.Name, userSignup)
+
+		_, err := r.Reconcile(req)
+		require.NoError(t, err)
+
+		// Confirm the UserSignup still exists
+		key := test.NamespacedName(test.HostOperatorNs, userSignup.Name)
+		require.NoError(t, r.client.Get(context.Background(), key, userSignup))
+		require.NotNil(t, userSignup)
 	})
 
 }
