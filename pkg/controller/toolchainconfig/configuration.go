e449737fb674478e8291aec657226610a95b33ed
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-05-18 19:54:18 -0400

    Fix lint error

--- pkg/controller/toolchainconfig/configuration.go
+++ pkg/controller/toolchainconfig/configuration.go
@@ -6,43 +6,43 @@ type ToolchainConfig struct {
 	cfg *v1alpha1.ToolchainConfigSpec
 }
 
-func (c *ToolchainConfig) AutomaticApproval() AutoApprovalCfg {
-	return AutoApprovalCfg{c.cfg.Host.AutomaticApproval}
+func (c *ToolchainConfig) AutomaticApproval() AutoApprovalConfig {
+	return AutoApprovalConfig{c.cfg.Host.AutomaticApproval}
 }
 
-func (c *ToolchainConfig) Deactivation() DeactivationCfg {
-	return DeactivationCfg{c.cfg.Host.Deactivation}
+func (c *ToolchainConfig) Deactivation() DeactivationConfig {
+	return DeactivationConfig{c.cfg.Host.Deactivation}
 }
 
-type AutoApprovalCfg struct {
+type AutoApprovalConfig struct {
 	approval v1alpha1.AutomaticApprovalCfg
 }
 
-func (a AutoApprovalCfg) IsEnabled() bool {
+func (a AutoApprovalConfig) IsEnabled() bool {
 	return getBool(a.approval.Enabled, false)
 }
 
-func (a AutoApprovalCfg) ResourceCapacityThresholdDefault() int {
+func (a AutoApprovalConfig) ResourceCapacityThresholdDefault() int {
 	return getInt(a.approval.ResourceCapacityThreshold.DefaultThreshold, 0)
 }
 
-func (a AutoApprovalCfg) ResourceCapacityThresholdSpecificPerMemberCluster() map[string]int {
+func (a AutoApprovalConfig) ResourceCapacityThresholdSpecificPerMemberCluster() map[string]int {
 	return a.approval.ResourceCapacityThreshold.SpecificPerMemberCluster
 }
 
-func (a AutoApprovalCfg) MaxNumberOfUsersOverall() int {
+func (a AutoApprovalConfig) MaxNumberOfUsersOverall() int {
 	return getInt(a.approval.MaxNumberOfUsers.Overall, 0)
 }
 
-func (a AutoApprovalCfg) MaxNumberOfUsersSpecificPerMemberCluster() map[string]int {
+func (a AutoApprovalConfig) MaxNumberOfUsersSpecificPerMemberCluster() map[string]int {
 	return a.approval.MaxNumberOfUsers.SpecificPerMemberCluster
 }
 
-type DeactivationCfg struct {
+type DeactivationConfig struct {
 	dctv v1alpha1.DeactivationCfg
 }
 
-func (d DeactivationCfg) DeactivatingNotificationInDays() int {
+func (d DeactivationConfig) DeactivatingNotificationInDays() int {
 	return getInt(d.dctv.DeactivatingNotificationDays, 3)
 }
 
