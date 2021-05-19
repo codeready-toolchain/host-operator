b15a4b750426afd2556c52ee4ae8bd55073189ea
Xavier Coulon <xcoulon@redhat.com>
2021-05-19 17:26:00 +0200

    assert counter and metrics
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/controller/toolchainstatus/toolchainstatus_controller_test.go
+++ pkg/controller/toolchainstatus/toolchainstatus_controller_test.go
@@ -1206,9 +1206,19 @@ func TestSynchronizationWithCounter(t *testing.T) {
 				"2": 3, // was incremented by `counter.UpdateUsersPerActivationCounters(2)`
 				"3": 1,
 			})
-		AssertThatCounters(t).HaveMasterUserRecords(9).
+		AssertThatCounters(t).
+			HaveMasterUserRecords(9).
 			HaveUserAccountsForCluster("member-1", 7).
-			HaveUserAccountsForCluster("member-2", 2)
+			HaveUserAccountsForCluster("member-2", 2).
+			HaveMasterUserRecordsPerDomain(toolchainv1alpha1.Metric{
+				string(metrics.Internal): 1,
+				string(metrics.External): 8,
+			}).
+			HaveUsersPerActivations(toolchainv1alpha1.Metric{
+				"1": 5,
+				"2": 3,
+				"3": 1,
+			})
 		AssertMetricsGaugeEquals(t, 5, metrics.UsersPerActivationGaugeVec.WithLabelValues("1")) // 5 users signed up a 1 time
 		AssertMetricsGaugeEquals(t, 3, metrics.UsersPerActivationGaugeVec.WithLabelValues("2")) // 3 users signed up a 2 times
 		AssertMetricsGaugeEquals(t, 1, metrics.UsersPerActivationGaugeVec.WithLabelValues("3")) // 1 user signed up a 3 times
