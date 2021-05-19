d75c6278a233e9c211051b3d98dc986a2ebb4b9c
Xavier Coulon <xcoulon@redhat.com>
2021-05-19 11:26:49 +0200

    Update pkg/metrics/email_domain.go
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

--- pkg/metrics/email_domain.go
+++ pkg/metrics/email_domain.go
@@ -9,12 +9,7 @@ type Domain string
 const Internal Domain = "internal"
 const External Domain = "external"
 
-var internalDomainPattern *regexp.Regexp
-
-func init() {
-	// pattern to match email addresses in the form of `@redhat.com`, `@ibm.com` or subdomains (eg: `@fr.ibm.com`)
-	internalDomainPattern = regexp.MustCompile(`^.*@redhat.com$|^.*@((.+)\.)?ibm.com$`)
-}
+var internalDomainPattern = regexp.MustCompile(`^.*@redhat.com$|^.*@((.+)\.)?ibm.com$`)
 
 // GetEmailDomain retrieves the email address for the given UserSignup
 // returns the associated domain (`Internal` or `External`)
