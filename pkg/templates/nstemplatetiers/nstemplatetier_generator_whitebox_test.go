8cc43b479295586d1549787372c6fa8c781ce0b0
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-09-10 13:04:23 -0400

    Fix tests

--- pkg/templates/nstemplatetiers/nstemplatetier_generator_whitebox_test.go
+++ pkg/templates/nstemplatetiers/nstemplatetier_generator_whitebox_test.go
@@ -40,7 +40,7 @@ func TestLoadTemplatesByTiers(t *testing.T) {
 			tmpls, err := loadTemplatesByTiers(assets)
 			// then
 			require.NoError(t, err)
-			require.Len(t, tmpls, 8)
+			require.Len(t, tmpls, 7)
 			require.NotContains(t, "foo", tmpls) // make sure that the `foo: bar` entry was ignored
 			for _, tier := range []string{"advanced", "base", "baselarge", "baseextended", "baseextendedidling", "basedeactivationdisabled", "test"} {
 				t.Run(tier, func(t *testing.T) {
@@ -490,7 +490,7 @@ func assertClusterResourcesTemplate(t *testing.T, decoder runtime.Decoder, actua
 
 func assertDefaultObjects(t *testing.T, actual templatev1.Template, idlerParamValue, memoryLimit string) {
 	assert.Len(t, actual.Objects, 13)
-	containsObj(t, actual, clusterResourceQuotaComputeObj("20000m", "1750m", memoryLimit, "15Gi"))
+	containsObj(t, actual, clusterResourceQuotaComputeObj("20000m", "1750m", "15Gi"))
 	containsObj(t, actual, clusterResourceQuotaDeploymentsObj())
 	containsObj(t, actual, clusterResourceQuotaReplicasObj())
 	containsObj(t, actual, clusterResourceQuotaRoutesObj())
@@ -504,6 +504,8 @@ func assertDefaultObjects(t *testing.T, actual templatev1.Template, idlerParamVa
 	containsObj(t, actual, idlerObj("${USERNAME}-dev"))
 	containsObj(t, actual, idlerObj("${USERNAME}-stage"))
 	containsParam(t, actual, "IDLER_TIMEOUT_SECONDS", idlerParamValue)
+	containsParam(t, actual, "MEMORY_LIMIT", memoryLimit)
+	containsParam(t, actual, "MEMORY_REQUEST", memoryLimit)
 }
 
 func assertNamespaceTemplate(t *testing.T, decoder runtime.Decoder, actual templatev1.Template, tier, kind string) {
@@ -558,7 +560,7 @@ func assertNamespaceTemplate(t *testing.T, decoder runtime.Decoder, actual templ
 
 	// User Namespaces Network Policies
 	switch tier {
-	case "advanced", "base", "baseextended", "baseextendedidling", "basedeactivationdisabled":
+	case "advanced", "base", "baselarge", "baseextended", "baseextendedidling", "basedeactivationdisabled":
 		switch kind {
 		case "dev":
 			containsObj(t, actual, allowFromCRWPolicyObj(kind))
@@ -640,8 +642,8 @@ func limitRangeObj(kind, cpuLimit, memoryLimit, cpuRequest, memoryRequest string
 	return fmt.Sprintf(`{"apiVersion":"v1","kind":"LimitRange","metadata":{"name":"resource-limits","namespace":"${USERNAME}-%s"},"spec":{"limits":[{"default":{"cpu":"%s","memory":"%s"},"defaultRequest":{"cpu":"%s","memory":"%s"},"type":"Container"}]}}`, kind, cpuLimit, memoryLimit, cpuRequest, memoryRequest)
 }
 
-func clusterResourceQuotaComputeObj(cpuLimit, cpuRequest, memoryLimit, storageLimit string) string { // nolint: unparam
-	return fmt.Sprintf(`{"apiVersion":"quota.openshift.io/v1","kind":"ClusterResourceQuota","metadata":{"name":"for-${USERNAME}-compute"},"spec":{"quota":{"hard":{"count/persistentvolumeclaims":"5","limits.cpu":"%[1]s","limits.ephemeral-storage":"7Gi","limits.memory":"%[3]s","requests.cpu":"%[2]s","requests.ephemeral-storage":"7Gi","requests.memory":"%[3]s","requests.storage":"%[4]s"}},"selector":{"annotations":{"openshift.io/requester":"${USERNAME}"},"labels":null}}}`, cpuLimit, cpuRequest, memoryLimit, storageLimit)
+func clusterResourceQuotaComputeObj(cpuLimit, cpuRequest, storageLimit string) string { // nolint: unparam
+	return fmt.Sprintf(`{"apiVersion":"quota.openshift.io/v1","kind":"ClusterResourceQuota","metadata":{"name":"for-${USERNAME}-compute"},"spec":{"quota":{"hard":{"count/persistentvolumeclaims":"5","limits.cpu":"%[1]s","limits.ephemeral-storage":"7Gi","limits.memory":"${MEMORY_LIMIT}","requests.cpu":"%[2]s","requests.ephemeral-storage":"7Gi","requests.memory":"${MEMORY_REQUEST}","requests.storage":"%[3]s"}},"selector":{"annotations":{"openshift.io/requester":"${USERNAME}"},"labels":null}}}`, cpuLimit, cpuRequest, storageLimit)
 }
 
 func clusterResourceQuotaDeploymentsObj() string {
