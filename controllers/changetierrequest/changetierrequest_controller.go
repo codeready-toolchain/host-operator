066e7e844a6ed0e0dca6dee1b01315f60fe647e2
Xavier Coulon <xcoulon@redhat.com>
2021-12-02 08:28:14 +0100

    Update controllers/changetierrequest/changetierrequest_controller.go
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

--- controllers/changetierrequest/changetierrequest_controller.go
+++ controllers/changetierrequest/changetierrequest_controller.go
@@ -178,7 +178,10 @@ func (r *Reconciler) changeTier(logger logr.Logger, changeTierRequest *toolchain
 			mur.Spec.UserAccounts[i].Spec.NSTemplateSet = newNsTemplateSet
 		}
 	}
-	mur.Spec.TierName = changeTierRequest.Spec.TierName
+	if mur.Spec.TierName != changeTierRequest.Spec.TierName {
+		mur.Spec.TierName = changeTierRequest.Spec.TierName
+		changed = true
+	}
 
 	if !changed {
 		err := fmt.Errorf("the MasterUserRecord '%s' doesn't contain UserAccount with cluster '%s' whose tier should be changed", changeTierRequest.Spec.MurName, changeTierRequest.Spec.TargetCluster)
