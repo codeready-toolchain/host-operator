8209346852a09873aae9741baa6c133ca98a8b10
Kanika Rana <krana@redhat.com>
2022-03-02 00:05:34 -0500

    add object type check in mapper

--- controllers/space/mapper.go
+++ controllers/space/mapper.go
@@ -1,15 +1,27 @@
 package space
 
 import (
+	"errors"
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
+var mapperLog = ctrl.Log.WithName("NSTemplateSetToSpaceMapper")
 
 func MapNSTemplateSetToSpace(hostNamespace string) func(object client.Object) []reconcile.Request {
 	return func(obj client.Object) []reconcile.Request {
-		return []reconcile.Request{{
-			NamespacedName: types.NamespacedName{Namespace: hostNamespace, Name: obj.GetName()},
-		}}
+		if nsTemplateSet, ok := obj.(*toolchainv1alpha1.NSTemplateSet); ok {
+			return []reconcile.Request{{
+				NamespacedName: types.NamespacedName{Namespace: hostNamespace, Name: nsTemplateSet.GetName()},
+			}}
+		}
+		// the obj was not a NSTemplateSet
+		mapperLog.Error(errors.New("not a NSTemplateSet"),
+			"MapNSTemplateSetToSpace attempted to map an object that wasn't a NSTemplateSet",
+			"obj", obj)
+		return []reconcile.Request{}
 	}
+
 }
--- controllers/space/mapper_test.go
+++ controllers/space/mapper_test.go
@@ -2,6 +2,7 @@ package space
 
 import (
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -28,4 +29,24 @@ func TestNSTemplateSetToSpaceMapper(t *testing.T) {
 		}, req[0].NamespacedName)
 	})
 
+	t.Run("test non-NSTemplateSet doesn't map", func(t *testing.T) {
+		// given
+		mur := &toolchainv1alpha1.MasterUserRecord{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              "echo",
+				Namespace:         test.HostOperatorNs,
+				CreationTimestamp: metav1.Now(),
+			},
+			Spec: toolchainv1alpha1.MasterUserRecordSpec{
+				UserID: "echo",
+			},
+			Status: toolchainv1alpha1.MasterUserRecordStatus{},
+		}
+
+		// when
+		req := MapNSTemplateSetToSpace("host-operator")(mur)
+
+		// then
+		require.Len(t, req, 0)
+	})
 }
