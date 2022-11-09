11060781cc14123994dac689cac2cc8c49c1a946
Francisc Munteanu <fmuntean@redhat.com>
2022-11-09 09:59:10 +0100

    set back master branch for cicd scripts

--- make/run-cicd-script.mk
+++ make/run-cicd-script.mk
@@ -1,4 +1,4 @@
-OWNER_AND_BRANCH_LOCATION=mfrancisc/toolchain-cicd/build_platform_config
+OWNER_AND_BRANCH_LOCATION=codeready-toolchain/toolchain-cicd/master
 GH_SCRIPTS_URL=https://raw.githubusercontent.com/${OWNER_AND_BRANCH_LOCATION}
 
 .PHONY: run-cicd-script
