764c6b6044a484618fdb666af615c320b9962645
Francisc Munteanu <fmuntean@redhat.com>
2022-12-22 12:29:57 +0100

    change make-bundle to handle eventual error from kustomize build command

--- make/manifests.mk
+++ make/manifests.mk
@@ -37,7 +37,10 @@ generate-rbac: build controller-gen
 .PHONY: bundle
 bundle: clean-bundle generate-rbac kustomize ## Generate bundle manifests and metadata, then validate generated files.
 	operator-sdk generate kustomize manifests -q
-	$(KUSTOMIZE) build config/manifests | operator-sdk generate bundle --overwrite --version=${NEXT_VERSION} --channels ${CHANNEL} --default-channel ${CHANNEL} --package toolchain-host-operator
+	$(eval TMP_MANIFEST_FILE := $(shell mktemp))
+	@echo "generating manifests to temporary file: ${TMP_MANIFEST_FILE}"
+	$(KUSTOMIZE) build config/manifests -o ${TMP_MANIFEST_FILE}
+	cat ${TMP_MANIFEST_FILE} | operator-sdk generate bundle --overwrite --version=${NEXT_VERSION} --channels ${CHANNEL} --default-channel ${CHANNEL} --package toolchain-host-operator
 	operator-sdk bundle validate ./bundle
 
 .PHONY: publish-current-bundle
