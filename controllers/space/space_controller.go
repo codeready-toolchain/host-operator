26423d6ca027b82a865ad89d6fb48f82972e2978
Xavier Coulon <xcoulon@redhat.com>
2023-03-06 11:30:50 +0100

    update comment
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- controllers/space/space_controller.go
+++ controllers/space/space_controller.go
@@ -341,7 +341,7 @@ func (r *Reconciler) manageNSTemplateSet(logger logr.Logger, space *toolchainv1a
 			nsTmplSet = NewNSTemplateSet(memberCluster.OperatorNamespace, space, spaceBindings.Items, tmplTier)
 			if err := memberCluster.Client.Create(context.TODO(), nsTmplSet); err != nil {
 				if errors.IsAlreadyExists(err) {
-					// requeue immediately, there's probably a race condition between the host client cache and the member cluster state
+					// requeue, there's probably a race condition between the host client cache and the member cluster state
 					return nsTmplSet, requeueDelay, nil
 				}
 				return nsTmplSet, norequeue, r.setStatusNSTemplateSetCreationFailed(logger, space, err)
