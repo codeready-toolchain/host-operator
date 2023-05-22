39be022c3b6bdf4cfdc9e080d1fdf8ed0b17e3dc
Francisc Munteanu <fmuntean@redhat.com>
2023-05-22 10:24:31 +0200

    fix: ldflags commit id and build time in github (#810)
    * fix ldflags commit id and build time in github

--- make/go.mk
+++ make/go.mk
@@ -1,5 +1,5 @@
 # By default the project should be build under GOPATH/src/github.com/<orgname>/<reponame>
-GO_PACKAGE_ORG_NAME ?= $(shell basename $$(dirname $$PWD))
+GO_PACKAGE_ORG_NAME ?= codeready-toolchain
 GO_PACKAGE_REPO_NAME ?= $(shell basename $$PWD)
 GO_PACKAGE_PATH ?= github.com/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}
 
