ad3ba2143891778cff5e5c87fa8b552c0772a9e6
Francisc Munteanu <fmuntean@redhat.com>
2023-08-14 22:14:32 +0200

    add sbr cleanup support

--- controllers/spacebindingcleanup/spacebinding_cleanup_controller.go
+++ controllers/spacebindingcleanup/spacebinding_cleanup_controller.go
@@ -99,33 +99,35 @@ func (r *Reconciler) deleteSpaceBinding(logger logr.Logger, spaceBinding *toolch
 
 	// check if spaceBinding was created from SpaceBindingRequest,
 	// in that case we must delete the SBR and will take care of deleting the SpaceBinding as well
-	sbrName, sbrNameFound := spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestLabelKey]
-	sbrNamespace, sbrNamespaceFound := spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestLabelKey]
-	if sbrNameFound && sbrNamespaceFound {
-		return r.deleteSpaceBindingRequest(logger, sbrNamespace, sbrName)
+	spaceBindingReqeuestAssociated := checkSpaceBindingRequestAssociated(spaceBinding)
+	if spaceBindingReqeuestAssociated.found {
+		return r.deleteSpaceBindingRequest(logger, spaceBindingReqeuestAssociated)
 	}
 
 	// otherwise delete the SpaceBinding resource directly ...
+	return r.DeleteSpaceBinding(spaceBinding)
+}
+
+func (r *Reconciler) DeleteSpaceBinding(spaceBinding *toolchainv1alpha1.SpaceBinding) error {
 	if err := r.Delete(context.TODO(), spaceBinding); err != nil {
 		return errs.Wrapf(err, "unable to delete the SpaceBinding")
 	}
 	return nil
 }
 
-func (r *Reconciler) deleteSpaceBindingRequest(logger logr.Logger, sbrNamespace, sbrName string) error {
+func (r *Reconciler) deleteSpaceBindingRequest(logger logr.Logger, sbrAssociated *SpaceBindingRequestAssociated) error {
 	spaceBindingRequest := &toolchainv1alpha1.SpaceBindingRequest{}
 	var memberClusterWithSpaceBindingRequest cluster.Cluster
 	var err error
 	for _, memberCluster := range r.MemberClusters {
 		err = memberCluster.Client.Get(context.TODO(), types.NamespacedName{
-			Namespace: sbrNamespace,
-			Name:      sbrName,
+			Namespace: sbrAssociated.namespace,
+			Name:      sbrAssociated.name,
 		}, spaceBindingRequest)
 		if err != nil {
 			if !errors.IsNotFound(err) {
 				// Error reading the object - requeue the request.
-				logger.Error(err, "unable to get the current SpaceBindingRequest", "SpaceBindingRequest.Name", sbrName, "SpaceBindingRequest.Namespace", sbrNamespace)
-				return err
+				return errs.Wrap(err, "unable to get the current SpaceBindingRequest")
 			}
 
 			//  spacebindingrequest CR not found on current membercluster
@@ -140,8 +142,9 @@ func (r *Reconciler) deleteSpaceBindingRequest(logger logr.Logger, sbrNamespace,
 	// it means that we couldn't find the spacebindingrequest object on any of the given member clusters
 	if err != nil && errors.IsNotFound(err) {
 		// let's just log the info
-		logger.Info("unable to find SpaceBindingRequest", "SpaceBindingRequest.Name", sbrName, "SpaceBindingRequest.Namespace", sbrNamespace)
-		return nil
+		logger.Info("unable to find SpaceBindingRequest", "SpaceBindingRequest.Name", sbrAssociated.name, "SpaceBindingRequest.Namespace", sbrAssociated.namespace)
+		// try and delete the SpaceBinding since SBR was not found
+		return r.DeleteSpaceBinding(sbrAssociated.spaceBinding)
 	}
 
 	// delete the SBR
@@ -149,4 +152,27 @@ func (r *Reconciler) deleteSpaceBindingRequest(logger logr.Logger, sbrNamespace,
 		return errs.Wrapf(err, "unable to delete the SpaceBindingRequest")
 	}
 	return nil
+}
+
+// SpaceBindingRequestAssociated is a wrapper that holds details regarding SB, and it's SBR if there is any associated.
+type SpaceBindingRequestAssociated struct {
+	// name and namespace of the SpaceBindingRequest
+	name, namespace string
+	// found is true if there is a SpaceBindingRequest associated with the SB
+	found bool
+	// spaceBinding is the resource that is being reconciled
+	spaceBinding *toolchainv1alpha1.SpaceBinding
+}
+
+func checkSpaceBindingRequestAssociated(spaceBinding *toolchainv1alpha1.SpaceBinding) *SpaceBindingRequestAssociated {
+	sbrName, sbrNameFound := spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestLabelKey]
+	sbrNamespace, sbrNamespaceFound := spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestNamespaceLabelKey]
+	// if both are found then there is a SBR associated with this spacebinding
+	hasSpaceBinding := sbrNamespaceFound && sbrNameFound
+	return &SpaceBindingRequestAssociated{
+		name:         sbrName,
+		namespace:    sbrNamespace,
+		found:        hasSpaceBinding,
+		spaceBinding: spaceBinding,
+	}
 }
--- controllers/spacebindingcleanup/spacebinding_cleanup_controller_test.go
+++ controllers/spacebindingcleanup/spacebinding_cleanup_controller_test.go
@@ -7,14 +7,19 @@ import (
 
 	"github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/host-operator/pkg/apis"
+	"github.com/codeready-toolchain/host-operator/pkg/cluster"
+	. "github.com/codeready-toolchain/host-operator/test"
 	sb "github.com/codeready-toolchain/host-operator/test/spacebinding"
+	spacebindingrequesttest "github.com/codeready-toolchain/host-operator/test/spacebindingrequest"
+	commoncluster "github.com/codeready-toolchain/toolchain-common/pkg/cluster"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test/masteruserrecord"
 	spacetest "github.com/codeready-toolchain/toolchain-common/pkg/test/space"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/stretchr/testify/require"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -34,10 +39,11 @@ func TestDeleteSpaceBinding(t *testing.T) {
 
 	t.Run("lara-redhat SpaceBinding removed when redhat space is missing", func(t *testing.T) {
 
-		reconciler, request, fakeClient := prepareReconciler(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace)
+		fakeClient := test.NewFakeClient(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace)
+		reconciler := prepareReconciler(t, fakeClient)
 
 		// when
-		_, err := reconciler.Reconcile(context.TODO(), request)
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraRedhatAdmin))
 
 		// then
 		require.NoError(t, err)
@@ -48,10 +54,11 @@ func TestDeleteSpaceBinding(t *testing.T) {
 
 	t.Run("joe-redhat SpaceBinding removed when joe MUR is missing", func(t *testing.T) {
 
-		reconciler, request, fakeClient := prepareReconciler(t, sbJoeRedhatView, sbLaraRedhatAdmin, sbLaraIbmEdit, laraMur, ibmSpace, redhatSpace)
+		fakeClient := test.NewFakeClient(t, sbJoeRedhatView, sbLaraRedhatAdmin, sbLaraIbmEdit, laraMur, ibmSpace, redhatSpace)
+		reconciler := prepareReconciler(t, fakeClient)
 
 		// when
-		_, err := reconciler.Reconcile(context.TODO(), request)
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbJoeRedhatView))
 
 		// then
 		require.NoError(t, err)
@@ -64,10 +71,11 @@ func TestDeleteSpaceBinding(t *testing.T) {
 		sbLaraRedhatAdmin := sbLaraRedhatAdmin.DeepCopy()
 		now := metav1.Now()
 		sbLaraRedhatAdmin.DeletionTimestamp = &now
-		reconciler, request, fakeClient := prepareReconciler(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace)
+		fakeClient := test.NewFakeClient(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace)
+		reconciler := prepareReconciler(t, fakeClient)
 
 		// when
-		_, err := reconciler.Reconcile(context.TODO(), request)
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraRedhatAdmin))
 
 		// then
 		require.NoError(t, err)
@@ -78,10 +86,11 @@ func TestDeleteSpaceBinding(t *testing.T) {
 
 	t.Run("no SpaceBinding removed when MUR and Space are present", func(t *testing.T) {
 
-		reconciler, request, fakeClient := prepareReconciler(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace, redhatSpace)
+		fakeClient := test.NewFakeClient(t, sbLaraRedhatAdmin, sbJoeRedhatView, sbLaraIbmEdit, laraMur, joeMur, ibmSpace, redhatSpace)
+		reconciler := prepareReconciler(t, fakeClient)
 
 		// when
-		_, err := reconciler.Reconcile(context.TODO(), request)
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraRedhatAdmin))
 
 		// then
 		require.NoError(t, err)
@@ -93,7 +102,8 @@ func TestDeleteSpaceBinding(t *testing.T) {
 	t.Run("fails while getting the bound resource", func(t *testing.T) {
 
 		for _, boundResourceName := range []string{"lara", "redhat"} {
-			reconciler, request, fakeClient := prepareReconciler(t, sbLaraRedhatAdmin, redhatSpace, laraMur)
+			fakeClient := test.NewFakeClient(t, sbLaraRedhatAdmin, redhatSpace, laraMur)
+			reconciler := prepareReconciler(t, fakeClient)
 			fakeClient.MockGet = func(ctx context.Context, key runtimeclient.ObjectKey, obj runtimeclient.Object, opts ...runtimeclient.GetOption) error {
 				if key.Name == boundResourceName {
 					return fmt.Errorf("some error")
@@ -102,7 +112,7 @@ func TestDeleteSpaceBinding(t *testing.T) {
 			}
 
 			// when
-			_, err := reconciler.Reconcile(context.TODO(), request)
+			_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraRedhatAdmin))
 
 			// then
 			require.Error(t, err)
@@ -112,13 +122,14 @@ func TestDeleteSpaceBinding(t *testing.T) {
 
 	t.Run("fails while deleting the SpaceBinding", func(t *testing.T) {
 
-		reconciler, request, fakeClient := prepareReconciler(t, sbLaraRedhatAdmin, redhatSpace)
+		fakeClient := test.NewFakeClient(t, sbLaraRedhatAdmin, redhatSpace)
+		reconciler := prepareReconciler(t, fakeClient)
 		fakeClient.MockDelete = func(ctx context.Context, obj runtimeclient.Object, opts ...runtimeclient.DeleteOption) error {
 			return fmt.Errorf("some error")
 		}
 
 		// when
-		_, err := reconciler.Reconcile(context.TODO(), request)
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraRedhatAdmin))
 
 		// then
 		require.Error(t, err)
@@ -126,20 +137,123 @@ func TestDeleteSpaceBinding(t *testing.T) {
 	})
 }
 
-func prepareReconciler(t *testing.T, sb *v1alpha1.SpaceBinding, initObjects ...runtime.Object) (*Reconciler, reconcile.Request, *test.FakeClient) {
+func TestDeleteSpaceBindingRequest(t *testing.T) {
+	sbr := spacebindingrequesttest.NewSpaceBindingRequest("lara", "lara-tenant",
+		spacebindingrequesttest.WithMUR("lara"),
+		spacebindingrequesttest.WithSpaceRole("admin"))
+	sbLaraAdmin := sb.NewSpaceBinding("lara", "lara", "admin", sbr.GetName(), sb.WithSpaceBindingRequest(sbr)) // the spacebinding was created from spacebindingrequest
+	t.Run("SpaceBindingRequest is deleted", func(t *testing.T) {
+		// given
+		member1 := NewMemberClusterWithClient(test.NewFakeClient(t, sbr), "member-1", corev1.ConditionTrue)
+
+		fakeClient := test.NewFakeClient(t, sbLaraAdmin)
+		reconciler := prepareReconciler(t, fakeClient, member1)
+
+		// when
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraAdmin))
+
+		// then
+		require.NoError(t, err)
+		spacebindingrequesttest.AssertThatSpaceBindingRequest(t, sbr.GetNamespace(), sbr.GetName(), member1.Client).DoesNotExist() // spacebindingrequest was deleted
+	})
+
+	t.Run("spaceBinding is deleted when spaceBindingRequest is missing", func(t *testing.T) {
+		// given
+		member1 := NewMemberClusterWithClient(test.NewFakeClient(t), "member-1", corev1.ConditionTrue) // for some reason spacebindingrequest is gone from member cluster
+		fakeClient := test.NewFakeClient(t, sbLaraAdmin)
+		reconciler := prepareReconciler(t, fakeClient, member1)
+
+		// when
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraAdmin))
+
+		// then
+		require.NoError(t, err)
+		sb.AssertThatSpaceBinding(t, test.HostOperatorNs, "lara", "lara", fakeClient).DoesNotExist() // the spacebinding is deleted
+	})
+
+	t.Run("unable to get SpaceBindingRequest", func(t *testing.T) {
+		// given
+		member1Client := test.NewFakeClient(t)
+		member1Client.MockGet = func(ctx context.Context, key runtimeclient.ObjectKey, obj runtimeclient.Object, opts ...runtimeclient.GetOption) error {
+			if _, ok := obj.(*v1alpha1.SpaceBindingRequest); ok {
+				return fmt.Errorf("mock error")
+			}
+			return member1Client.Get(ctx, key, obj, opts...)
+		}
+		member1 := NewMemberClusterWithClient(member1Client, "member-1", corev1.ConditionTrue) // for some reason spacebindingrequest is gone from member cluster
+		fakeClient := test.NewFakeClient(t, sbLaraAdmin)
+		reconciler := prepareReconciler(t, fakeClient, member1)
+
+		// when
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraAdmin))
+
+		// then
+		require.EqualError(t, err, "unable to get the current SpaceBindingRequest: mock error")
+		sb.AssertThatSpaceBinding(t, test.HostOperatorNs, "lara", "lara", fakeClient).Exists() // the spacebinding is not deleted yet
+	})
+
+	t.Run("fails while deleting the SpaceBindingRequest", func(t *testing.T) {
+		// given
+		member1Client := test.NewFakeClient(t, sbr)
+		member1Client.MockDelete = func(ctx context.Context, obj runtimeclient.Object, opts ...runtimeclient.DeleteOption) error {
+			if _, ok := obj.(*v1alpha1.SpaceBindingRequest); ok {
+				return fmt.Errorf("mock error")
+			}
+			return member1Client.Client.Delete(ctx, obj, opts...)
+		}
+		member1 := NewMemberClusterWithClient(member1Client, "member-1", corev1.ConditionTrue) // for some reason spacebindingrequest is gone from member cluster
+		fakeClient := test.NewFakeClient(t, sbLaraAdmin)
+		reconciler := prepareReconciler(t, fakeClient, member1)
+
+		// when
+		_, err := reconciler.Reconcile(context.TODO(), requestFor(sbLaraAdmin))
+
+		// then
+		require.EqualError(t, err, "unable to delete the SpaceBindingRequest: mock error")
+		sb.AssertThatSpaceBinding(t, test.HostOperatorNs, "lara", "lara", fakeClient).Exists() // the spacebinding is not deleted yet
+	})
+
+}
+
+func prepareReconciler(t *testing.T, hostCl runtimeclient.Client, memberClusters ...*commoncluster.CachedToolchainCluster) *Reconciler {
 	s := scheme.Scheme
 	err := apis.AddToScheme(s)
 	require.NoError(t, err)
 
-	cl := test.NewFakeClient(t, append(initObjects, sb)...)
+	clusters := map[string]cluster.Cluster{}
+	for _, c := range memberClusters {
+		clusters[c.Name] = cluster.Cluster{
+			Config: &commoncluster.Config{
+				Type:              commoncluster.Member,
+				OperatorNamespace: c.OperatorNamespace,
+				OwnerClusterName:  test.MemberClusterName,
+			},
+			Client: c.Client,
+		}
+	}
 
 	reconciler := &Reconciler{
-		Namespace: test.HostOperatorNs,
-		Scheme:    s,
-		Client:    cl,
+		Namespace:      test.HostOperatorNs,
+		Scheme:         s,
+		Client:         hostCl,
+		MemberClusters: clusters,
 	}
-	req := reconcile.Request{
-		NamespacedName: test.NamespacedName(test.HostOperatorNs, sb.Name),
+	return reconciler
+}
+
+func requestFor(s *v1alpha1.SpaceBinding) reconcile.Request {
+	if s != nil {
+		return reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: s.Namespace,
+				Name:      s.Name,
+			},
+		}
 	}
-	return reconciler, req, cl
+	return reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: "john-tenant",
+			Name:      "unknown",
+		},
+	}
 }
