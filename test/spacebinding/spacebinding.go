6c9e07da3665f542448ff46fc91d7fa1bfd83853
Francisc Munteanu <fmuntean@redhat.com>
2023-08-23 13:43:09 +0200

    feat: Implement SpaceBindingRequest controller main logic (#848)
    * implement SBR main logic
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

--- test/spacebinding/spacebinding.go
+++ test/spacebinding/spacebinding.go
@@ -2,14 +2,18 @@ package spacebinding
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/codeready-toolchain/api/api/v1alpha1"
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func NewSpaceBinding(mur, space, spaceRole, creator string) *v1alpha1.SpaceBinding {
-	return &v1alpha1.SpaceBinding{
+type Option func(spaceRequest *toolchainv1alpha1.SpaceBinding)
+
+func NewSpaceBinding(mur, space, spaceRole, creator string, options ...Option) *v1alpha1.SpaceBinding {
+	sb := &v1alpha1.SpaceBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s-%s", mur, space),
 			Namespace: test.HostOperatorNs,
@@ -24,5 +28,24 @@ func NewSpaceBinding(mur, space, spaceRole, creator string) *v1alpha1.SpaceBindi
 			Space:            space,
 			SpaceRole:        spaceRole,
 		},
+	}
+
+	for _, apply := range options {
+		apply(sb)
+	}
+	return sb
+}
+
+func WithSpaceBindingRequest(sbr *toolchainv1alpha1.SpaceBindingRequest) Option {
+	return func(spaceBinding *toolchainv1alpha1.SpaceBinding) {
+		spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestLabelKey] = sbr.Name
+		spaceBinding.Labels[toolchainv1alpha1.SpaceBindingRequestNamespaceLabelKey] = sbr.Namespace
+	}
+}
+
+func WithDeletionTimestamp() Option {
+	return func(spaceBinding *toolchainv1alpha1.SpaceBinding) {
+		now := metav1.NewTime(time.Now())
+		spaceBinding.DeletionTimestamp = &now
 	}
 }
--- test/spacerequest/spacerequest.go
+++ test/spacerequest/spacerequest.go
@@ -1,10 +1,12 @@
 package spacerequest
 
 import (
+	"fmt"
 	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/gofrs/uuid"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -60,4 +62,22 @@ func WithStatusNamespaceAccess(namespaceAccess toolchainv1alpha1.NamespaceAccess
 	return func(spaceRequest *toolchainv1alpha1.SpaceRequest) {
 		spaceRequest.Status.NamespaceAccess = []toolchainv1alpha1.NamespaceAccess{namespaceAccess}
 	}
+}
+
+func NewNamespace(spacename string) *corev1.Namespace {
+	labels := map[string]string{
+		toolchainv1alpha1.TypeLabelKey:     "tenant",
+		toolchainv1alpha1.ProviderLabelKey: toolchainv1alpha1.ProviderLabelValue,
+	}
+	if spacename != "nospace" {
+		labels[toolchainv1alpha1.SpaceLabelKey] = spacename
+	}
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   fmt.Sprintf("%s-tenant", spacename),
+			Labels: labels,
+		},
+		Status: corev1.NamespaceStatus{Phase: corev1.NamespaceActive},
+	}
+	return ns
 }
