8498692d18dbd6ba54b201a6e36d105b6a4cdb07
Martin Mulholland <mmulholl@redhat.com>
2023-10-18 09:18:29 -0400

    solution

--- make/test.mk
+++ make/test.mk
@@ -101,23 +101,13 @@ ifeq ($(E2E_REPO_PATH),"")
 			$(eval AUTHOR_LINK = https://github.com/${AUTHOR})
         else
 			$(eval AUTHOR_LINK = $(shell jq -r '.refs[0].pulls[0].author_link' <<< $${CLONEREFS_OPTIONS} | tr -d '[:space:]'))
-			# get branch ref of the fork the PR was created from
 			@echo "found author link ${AUTHOR_LINK}"
 			$(eval BRANCH_NAME := $(shell jq -r '.refs[0].pulls[0].head_ref' <<< $${CLONEREFS_OPTIONS} | tr -d '[:space:]'))
-			@echo "found branch NAME ${BRANCH_NAME}"
-			# It is possible the head_ref value is not set
-			if [[ -z "${BRANCH_NAME}" ]]; then \
-				@echo "using pull sha ${PULL_PULL_SHA}"; \
-            	# get branch ref of the fork the PR was created from \
-				$(eval BRANCH_REF := $(shell curl ${AUTHOR_LINK}/host-operator.git/info/refs?service=git-upload-pack --output - /dev/null 2>&1 | grep -a ${PULL_PULL_SHA}.*refs/heads/ | awk '{print $$2}')); \
-				$(eval BRANCH_NAME := $(shell echo ${BRANCH_REF} | awk -F'/' '{print $$3}')); \
-				@echo "found branch name ${BRANCH_NAME}"; \
-			fi;
         endif
 		@echo "using author link ${AUTHOR_LINK}"
 		@echo "detected branch ${BRANCH_NAME}"
 		# check if a branch with the same ref exists in the user's fork of toolchain-e2e repo
-		$(eval REMOTE_E2E_BRANCH := $(shell curl ${AUTHOR_LINK}/toolchain-e2e.git/info/refs?service=git-upload-pack --output - 2>/dev/null | grep -a "${BRANCH_REF}$$" | awk '{print $$2}'))
+		$(eval REMOTE_E2E_BRANCH := $(shell curl ${AUTHOR_LINK}/toolchain-e2e.git/info/refs?service=git-upload-pack --output - 2>/dev/null | grep -a "refs/heads/${BRANCH_NAME}$$" | awk '{print $$2}'))
 		@echo "branch ref of the user's fork: \"${REMOTE_E2E_BRANCH}\" - if empty then not found"
 		# check if the branch with the same name exists, if so then merge it with master and use the merge branch, if not then use master
 		if [[ -n "${REMOTE_E2E_BRANCH}" ]]; then \
@@ -126,7 +116,7 @@ ifeq ($(E2E_REPO_PATH),"")
 			# add the user's fork as remote repo \
 			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} remote add external ${AUTHOR_LINK}/toolchain-e2e.git; \
 			# fetch the branch \
-			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} fetch external refs/heads/${BRANCH_NAME}; \
+			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} fetch external ${REMOTE_E2E_BRANCH}; \
 			# merge the branch with master \
 			git --git-dir=${E2E_REPO_PATH}/.git --work-tree=${E2E_REPO_PATH} merge --allow-unrelated-histories --no-commit FETCH_HEAD; \
 		fi;
