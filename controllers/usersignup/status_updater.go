13ffc9992d69940eb7b199f14f87481dcb470332
Baiju Muthukadan <baiju.m.mail@gmail.com>
2023-10-31 13:05:55 +0530

    Update controllers/usersignup/status_updater.go
    Co-authored-by: Alexey Kazakov <alkazako@redhat.com>

--- controllers/usersignup/status_updater.go
+++ controllers/usersignup/status_updater.go
@@ -442,6 +442,9 @@ func (u *StatusUpdater) updateStatusConditions(ctx context.Context, userSignup *
 }
 
 func (u *StatusUpdater) updateStatusHomeSpace(ctx context.Context, userSignup *toolchainv1alpha1.UserSignup, spaceName string) error {
-	userSignup.Status.HomeSpace = spaceName
-	return u.Client.Status().Update(ctx, userSignup)
+	if userSignup.Status.HomeSpace != spaceName {
+		userSignup.Status.HomeSpace = spaceName
+		return u.Client.Status().Update(ctx, userSignup)
+	}
+	return nil // Nothing changed
 }
