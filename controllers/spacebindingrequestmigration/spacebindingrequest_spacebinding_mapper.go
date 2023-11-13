c86c05cade77cd893076a8a0a9088b0842015273
Francisc Munteanu <fmuntean@redhat.com>
2023-11-13 15:35:39 +0100

    remove cast in mapper

--- controllers/spacebindingrequestmigration/spacebindingrequest_spacebinding_mapper.go
+++ controllers/spacebindingrequestmigration/spacebindingrequest_spacebinding_mapper.go
@@ -16,38 +16,35 @@ import (
 func MapSpaceBindingRequestToSpaceBinding(cl runtimeclient.Client) func(spaceBindingREquest runtimeclient.Object) []reconcile.Request {
 	mapperLog := ctrl.Log.WithName("NSTemplateTierToSpaceMapper")
 	return func(obj runtimeclient.Object) []reconcile.Request {
-		if spaceBindingRequest, ok := obj.(*toolchainv1alpha1.SpaceBindingRequest); ok {
-			watchNamespace, err := commonconfig.GetWatchNamespace()
-			if err != nil {
-				mapperLog.Error(err, "cannot get watch namespace")
-				return []reconcile.Request{}
-			}
-			spaceBindings := &toolchainv1alpha1.SpaceBindingList{}
-			err = cl.List(context.TODO(), spaceBindings,
-				runtimeclient.InNamespace(watchNamespace),
-				runtimeclient.MatchingLabels{
-					toolchainv1alpha1.SpaceBindingRequestLabelKey:          spaceBindingRequest.GetName(),
-					toolchainv1alpha1.SpaceBindingRequestNamespaceLabelKey: spaceBindingRequest.GetNamespace(),
-				})
-			if err != nil {
-				mapperLog.Error(err, "cannot list spacebindings")
-				return []reconcile.Request{}
-			}
-			if len(spaceBindings.Items) == 0 {
-				return []reconcile.Request{}
-			}
-
-			req := make([]reconcile.Request, len(spaceBindings.Items))
-			for i, item := range spaceBindings.Items {
-				req[i] = reconcile.Request{
-					NamespacedName: types.NamespacedName{
-						Namespace: item.Namespace,
-						Name:      item.Name,
-					},
-				}
-			}
-			return req
+		watchNamespace, err := commonconfig.GetWatchNamespace()
+		if err != nil {
+			mapperLog.Error(err, "cannot get watch namespace")
+			return []reconcile.Request{}
 		}
-		return []reconcile.Request{}
+		spaceBindings := &toolchainv1alpha1.SpaceBindingList{}
+		err = cl.List(context.TODO(), spaceBindings,
+			runtimeclient.InNamespace(watchNamespace),
+			runtimeclient.MatchingLabels{
+				toolchainv1alpha1.SpaceBindingRequestLabelKey:          obj.GetName(),
+				toolchainv1alpha1.SpaceBindingRequestNamespaceLabelKey: obj.GetNamespace(),
+			})
+		if err != nil {
+			mapperLog.Error(err, "cannot list spacebindings")
+			return []reconcile.Request{}
+		}
+		if len(spaceBindings.Items) == 0 {
+			return []reconcile.Request{}
+		}
+
+		req := make([]reconcile.Request, len(spaceBindings.Items))
+		for i, item := range spaceBindings.Items {
+			req[i] = reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: item.Namespace,
+					Name:      item.Name,
+				},
+			}
+		}
+		return req
 	}
 }
