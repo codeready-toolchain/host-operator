32ccaf808e90b3574a960bf643bc7a2ed12d83a0
Lukas Krejci <lkrejci@redhat.com>
2024-01-22 18:27:49 +0100

    Add tests for error conditions and remove the duplicate test.

--- controllers/spaceprovisionerconfig/spaceprovisionerconfig_controller_test.go
+++ controllers/spaceprovisionerconfig/spaceprovisionerconfig_controller_test.go
@@ -2,8 +2,10 @@ package spaceprovisionerconfig
 
 import (
 	"context"
+	"errors"
 	"os"
 	"testing"
+	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-common/pkg/apis"
@@ -11,6 +13,7 @@ import (
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -19,7 +22,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func TestSpaceProvisionerConfig(t *testing.T) {
+func TestSpaceProvisionerConfigValidation(t *testing.T) {
 	t.Run("is not valid when non-existing ToolchainCluster is referenced", func(t *testing.T) {
 		// given
 		spc := &toolchainv1alpha1.SpaceProvisionerConfig{
@@ -72,52 +75,74 @@ func TestSpaceProvisionerConfig(t *testing.T) {
 	})
 }
 
-func TestEnqueingFilter(t *testing.T) {
-	// given
-	cl := test.NewFakeClient(t, &toolchainv1alpha1.SpaceProvisionerConfig{
+func TestSpaceProvisionerConfigReEnqueing(t *testing.T) {
+	spc := &toolchainv1alpha1.SpaceProvisionerConfig{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "spc0",
+			Name:      "spc",
 			Namespace: test.HostOperatorNs,
 		},
 		Spec: toolchainv1alpha1.SpaceProvisionerConfigSpec{
 			ToolchainCluster: "cluster1",
 		},
-	}, &toolchainv1alpha1.SpaceProvisionerConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "spc1",
-			Namespace: test.HostOperatorNs,
-		},
-		Spec: toolchainv1alpha1.SpaceProvisionerConfigSpec{
-			ToolchainCluster: "cluster2",
-		},
-	}, &toolchainv1alpha1.SpaceProvisionerConfig{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "spc2",
-			Namespace: test.HostOperatorNs,
-		},
-		Spec: toolchainv1alpha1.SpaceProvisionerConfigSpec{
-			ToolchainCluster: "cluster1",
-		},
+	}
+
+	t.Run("re-enqueues on failure to GET", func(t *testing.T) {
+		// given
+		r, req, cl := prepareReconcile(t, spc.DeepCopy())
+
+		expectedErr := errors.New("purposefully failing the get request")
+		cl.MockGet = func(ctx context.Context, key runtimeclient.ObjectKey, obj runtimeclient.Object, opts ...runtimeclient.GetOption) error {
+			return expectedErr
+		}
+
+		// when
+		_, reconcileErr := r.Reconcile(context.TODO(), req)
+
+		// then
+		assert.ErrorIs(t, reconcileErr, expectedErr)
 	})
+	t.Run("re-enqueues on failure to update the status", func(t *testing.T) {
+		// given
+		r, req, cl := prepareReconcile(t, spc.DeepCopy())
 
-	// when
-	reqs, err := findReferencingProvisionerConfigs(context.TODO(), cl, runtimeclient.ObjectKey{Name: "cluster1", Namespace: test.HostOperatorNs})
+		expectedErr := errors.New("purposefully failing the get request")
+		cl.MockStatusUpdate = func(ctx context.Context, obj runtimeclient.Object, opts ...runtimeclient.UpdateOption) error {
+			return expectedErr
+		}
 
-	// then
-	require.NoError(t, err)
-	require.Len(t, reqs, 2)
-	assert.Equal(t, reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Namespace: test.HostOperatorNs,
-			Name:      "spc0",
-		},
-	}, reqs[0])
-	assert.Equal(t, reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Namespace: test.HostOperatorNs,
-			Name:      "spc2",
-		},
-	}, reqs[1])
+		// when
+		_, reconcileErr := r.Reconcile(context.TODO(), req)
+
+		// then
+		assert.ErrorIs(t, reconcileErr, expectedErr)
+	})
+	t.Run("doesn't re-enqueue when object not found", func(t *testing.T) {
+		// given
+		r, req, cl := prepareReconcile(t, spc.DeepCopy())
+
+		cl.MockGet = func(ctx context.Context, key runtimeclient.ObjectKey, obj runtimeclient.Object, opts ...runtimeclient.GetOption) error {
+			return &kerrors.StatusError{ErrStatus: metav1.Status{Reason: metav1.StatusReasonNotFound}}
+		}
+
+		// when
+		_, reconcileErr := r.Reconcile(context.TODO(), req)
+
+		// then
+		assert.NoError(t, reconcileErr)
+	})
+	t.Run("doesn't re-enqueue when object being deleted", func(t *testing.T) {
+		// given
+		spc := spc.DeepCopy()
+		spc.SetDeletionTimestamp(&metav1.Time{Time: time.Now()})
+		r, req, _ := prepareReconcile(t, spc)
+
+		// when
+		_, reconcileErr := r.Reconcile(context.TODO(), req)
+
+		// then
+		assert.NoError(t, reconcileErr)
+		assert.Empty(t, spc.Status.Conditions)
+	})
 }
 
 func prepareReconcile(t *testing.T, spc *toolchainv1alpha1.SpaceProvisionerConfig, initObjs ...runtime.Object) (*Reconciler, reconcile.Request, *test.FakeClient) {
