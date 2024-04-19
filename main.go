f2c8937928476f860c819f43410eaf61f4c60cb3
Feny Mehta <fbm3307@gmail.com>
2024-04-19 14:51:09 +0530

    .rc-2
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- main.go
+++ main.go
@@ -230,7 +230,7 @@ func main() { // nolint:gocyclo
 		Scheme:     mgr.GetScheme(),
 		RequeAfter: 10 * time.Second,
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "ToolchainClusterHealth")
+		setupLog.Error(err, "unable to create controller", "controller", "ToolchainCluster")
 		os.Exit(1)
 	}
 
