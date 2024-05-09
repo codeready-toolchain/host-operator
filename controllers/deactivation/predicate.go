5879cab4653f408b2514eb9666238ec7f905ad20
Shane Bryzak <sbryzak@gmail.com>
2024-05-09 16:27:51 +1000

    updated comments

--- controllers/deactivation/predicate.go
+++ controllers/deactivation/predicate.go
@@ -31,11 +31,13 @@ func (CreateAndUpdateOnlyPredicate) Generic(_ event.GenericEvent) bool {
 	return false
 }
 
+// GenerationOrConditionsChangedPredicate is a predicate that will fire on the generation change of a UserSignup, or if
+// the conditions contained in the Status are updated (ignoring any changes to the message property).
 type GenerationOrConditionsChangedPredicate struct {
 	predicate.GenerationChangedPredicate
 }
 
-// Update implements default UpdateEvent filter for validating no generation change
+// Update implements default UpdateEvent filter for validating no generation change or no change in conditions
 func (GenerationOrConditionsChangedPredicate) Update(e event.UpdateEvent) bool {
 	if e.ObjectOld == nil || e.ObjectNew == nil {
 		return false
