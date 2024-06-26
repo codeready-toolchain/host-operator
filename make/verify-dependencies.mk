d5d584bc2dd66701696fbb557504b8530a25138b
Feny Mehta <fbm3307@gmail.com>
2024-06-26 14:51:30 +0530

    rc
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/verify-dependencies.mk
+++ make/verify-dependencies.mk
@@ -1,6 +1,6 @@
 .PHONY: verify-dependencies
 ## Runs commands to verify after the updated dependecies of toolchain-common/API(go mod replace), if the repo needs any changes to be made
-verify-dependencies: tidy generate vet build test lint
+verify-dependencies: tidy generate vet build test lint-go-code
 
 .PHONY: tidy
 tidy: 
