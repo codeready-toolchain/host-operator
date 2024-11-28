d1c69e8ecf143bf573e720f997f3de6b39f07eb3
Devtools <devtools@redhat.com>
2024-11-28 09:39:06 +0100

    remove deprectaed errors pkg

--- controllers/nstemplatetier/nstemplatetier_controller.go
+++ controllers/nstemplatetier/nstemplatetier_controller.go
@@ -3,12 +3,11 @@ package nstemplatetier
 import (
 	"bytes"
 	"context"
+	errs "errors"
 
 	"fmt"
 	"io"
 	"text/template"
-
-	gherrors "github.com/pkg/errors"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/host-operator/controllers/toolchainconfig"
@@ -18,7 +17,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	errs "github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -69,18 +67,18 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		}
 		// Error reading the object - requeue the request.
 		logger.Error(err, "unable to get the current NSTemplateTier")
-		return reconcile.Result{}, errs.Wrap(err, "unable to get the current NSTemplateTier")
+		return reconcile.Result{}, fmt.Errorf("unable to get the current NSTemplateTier: %w", err)
 	}
 
 	_, err := toolchainconfig.GetToolchainConfig(r.Client)
 	if err != nil {
-		return reconcile.Result{}, errs.Wrapf(err, "unable to get ToolchainConfig")
+		return reconcile.Result{}, fmt.Errorf("unable to get ToolchainConfig: %w", err)
 	}
 
 	// create a new entry in the `status.history` if needed
 	if added, err := r.ensureStatusUpdateRecord(ctx, tier); err != nil {
 		logger.Error(err, "unable to insert a new entry in status.updates after NSTemplateTier changed")
-		return reconcile.Result{}, errs.Wrap(err, "unable to insert a new entry in status.updates after NSTemplateTier changed")
+		return reconcile.Result{}, fmt.Errorf("unable to insert a new entry in status.updates after NSTemplateTier changed: %w", err)
 	} else if added {
 		logger.Info("Requeue after adding a new entry in tier.status.updates")
 		return reconcile.Result{Requeue: true}, nil
@@ -88,7 +86,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 
 	// check if the `status.revisions` field is up-to-date and create a TTR for each TierTemplate
 	if created, err := r.ensureRevision(ctx, tier); err != nil {
-		return reconcile.Result{}, errs.Wrap(err, "unable to create new TierTemplateRevision after NSTemplateTier changed")
+		return reconcile.Result{}, fmt.Errorf("unable to create new TierTemplateRevision after NSTemplateTier changed: %w", err)
 	} else if created {
 		logger.Info("Requeue after creating a new TTR")
 		return reconcile.Result{Requeue: true}, nil
@@ -103,7 +101,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 func (r *Reconciler) ensureStatusUpdateRecord(ctx context.Context, tier *toolchainv1alpha1.NSTemplateTier) (bool, error) {
 	hash, err := hash.ComputeHashForNSTemplateTier(tier)
 	if err != nil {
-		return false, errs.Wrapf(err, "unable to append an entry in the `status.updates` for NSTemplateTier '%s'", tier.Name)
+		return false, fmt.Errorf("unable to append an entry in the `status.updates` for NSTemplateTier '%s' : %w", tier.Name, err)
 	}
 	// if there was no previous status:
 	if len(tier.Status.Updates) == 0 {
@@ -265,7 +263,7 @@ func getParsedObjectsFromBuffer(buf *bytes.Buffer, parsedObjects []runtime.RawEx
 	for {
 		var rawExt runtime.RawExtension
 		if err := decoder.Decode(&rawExt); err != nil {
-			if gherrors.Is(err, io.EOF) {
+			if errs.Is(err, io.EOF) {
 				break
 			}
 			return parsedObjects, err
@@ -325,7 +323,7 @@ func getNSTemplateTierRefs(tmplTier *toolchainv1alpha1.NSTemplateTier) []string 
 func (r *Reconciler) createTTR(ctx context.Context, ttr *toolchainv1alpha1.TierTemplateRevision, tmplTier *toolchainv1alpha1.TierTemplate) (*toolchainv1alpha1.TierTemplateRevision, error) {
 	err := r.Client.Create(ctx, ttr)
 	if err != nil && !errors.IsAlreadyExists(err) {
-		return nil, errs.Wrap(err, "unable to create TierTemplateRevision")
+		return nil, fmt.Errorf("unable to create TierTemplateRevision: %w", err)
 	}
 
 	logger := log.FromContext(ctx)
