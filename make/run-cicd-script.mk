47f96dd0fa7795d669b1a89242d4ce3e723e2935
Kanika Rana <krana@redhat.com>
2025-01-22 15:16:10 +0530

    check cicd changes

--- make/run-cicd-script.mk
+++ make/run-cicd-script.mk
@@ -1,4 +1,4 @@
-OWNER_AND_BRANCH_LOCATION=codeready-toolchain/toolchain-cicd/master
+OWNER_AND_BRANCH_LOCATION=ranakan19/toolchain-cicd/openshift_4_16_cicd
 GH_SCRIPTS_URL=https://raw.githubusercontent.com/${OWNER_AND_BRANCH_LOCATION}
 
 .PHONY: run-cicd-script
