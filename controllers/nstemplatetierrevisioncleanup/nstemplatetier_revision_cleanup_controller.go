b5534de2a994ee69ea2981493c28796bd023f189
Feny Mehta <fbm3307@gmail.com>
2025-03-07 15:42:01 +0530

    KUBESAW-264: Implement TierTemplateRevision cleanup controller
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- controllers/nstemplatetierrevisioncleanup/nstemplatetier_revision_cleanup_controller.go
+++ controllers/nstemplatetierrevisioncleanup/nstemplatetier_revision_cleanup_controller.go
@@ -8,23 +8,23 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/host-operator/controllers/nstemplatetier"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-var AgeOld = 24 * time.Hour
+const deletionTimeThreshold = 30 * time.Second
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *Reconciler) SetupWithManager(mgr manager.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&toolchainv1alpha1.NSTemplateTier{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
+		For(&toolchainv1alpha1.TierTemplateRevision{}).
 		Complete(r)
 }
 
@@ -37,8 +37,27 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 	logger := log.FromContext(ctx)
 
 	// fetch the NSTemplateTier tier
+	ttr := &toolchainv1alpha1.TierTemplateRevision{}
+	if err := r.Client.Get(ctx, request.NamespacedName, ttr); err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("TierTemplateRevision not found")
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		logger.Error(err, "unable to get the current TierTemplateRevision")
+		return reconcile.Result{}, fmt.Errorf("unable to get the current TierTemplateRevision: %w", err)
+	}
+	tierName, ok := ttr.GetLabels()[toolchainv1alpha1.TierLabelKey]
+	if !ok {
+		return reconcile.Result{}, fmt.Errorf("tier-name label not found in tiertemplaterevision")
+
+	}
+	// fetch the related NSTemplateTier tier
 	tier := &toolchainv1alpha1.NSTemplateTier{}
-	if err := r.Client.Get(ctx, request.NamespacedName, tier); err != nil {
+	if err := r.Client.Get(ctx, types.NamespacedName{
+		Namespace: ttr.GetNamespace(),
+		Name:      tierName,
+	}, tier); err != nil {
 		if errors.IsNotFound(err) {
 			logger.Info("NSTemplateTier not found")
 			return reconcile.Result{}, nil
@@ -48,68 +67,62 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		return reconcile.Result{}, fmt.Errorf("unable to get the current NSTemplateTier: %w", err)
 	}
 
-	// Get the list of tier template revisions which are unsused(not referenced by any tiers and whose creation date is older that 24hours(1 day))
-	ttrevisions, err := r.ListUnusedTTRs(ctx, tier, AgeOld)
+	// verify that the tier template revision which is unsused(not referenced by any tiers and whose creation date is older that 30secs
+	// and all the spaces are up-to-date)
+	UnusedRevisionBool, requeue, reqAft, err := r.VerifyUnusedTTR(ctx, tier, ttr, deletionTimeThreshold)
 	if err != nil {
-		return reconcile.Result{}, fmt.Errorf("unable to get the current unused Tier Template Revisions List: %w", err)
+		return reconcile.Result{RequeueAfter: reqAft, Requeue: requeue}, fmt.Errorf("cannot delete the current TTR,requing :%w", err)
 	}
 
-	// Delete the unused revisions
-	if err := r.DeleteRevision(ctx, ttrevisions); err != nil {
-		return reconcile.Result{}, fmt.Errorf("unable to delete the current Tier Template Revision: %w", err)
+	// Delete the unused revision
+	if UnusedRevisionBool {
+		if err := r.Client.Delete(ctx, ttr); err != nil {
+			return reconcile.Result{}, fmt.Errorf("unable to delete the current Tier Template Revision %s: %w", ttr.Name, err)
+		}
 	}
+
 	return reconcile.Result{}, nil
 }
 
-// ListTTRS function first lists all the tier template revisions, then compare it with the nsTemplatetierRevisions map list, and gets the revisions
-// which are no longer referenced/used by any space and whose creation time stamp is older than 1 day(24hours) and returns this list of unused revisions
-func (r *Reconciler) ListUnusedTTRs(ctx context.Context, nsTmplTier *toolchainv1alpha1.NSTemplateTier, ageOld time.Duration) ([]toolchainv1alpha1.TierTemplateRevision, error) {
+// VerifyUnusedTTR function verifies that the tier template revision's creation time stamp is older than 30sec,
+// it is not present/refernced in status.revisions field, and there are no outdated spaces.
+func (r *Reconciler) VerifyUnusedTTR(ctx context.Context, nsTmplTier *toolchainv1alpha1.NSTemplateTier,
+	rev *toolchainv1alpha1.TierTemplateRevision, deletionTimeThreshold time.Duration) (bool, bool, time.Duration, error) {
 
-	ttrDelList := []toolchainv1alpha1.TierTemplateRevision{}
-	ttr := toolchainv1alpha1.TierTemplateRevision{}
-	ttRevList := toolchainv1alpha1.TierTemplateRevisionList{}
-	unUsedRevisions := make(map[string]bool)
+	//get the tier template revision creation time stamp and the duration
+	timeSinceCreation := time.Since(rev.GetCreationTimestamp().Time)
 
-	//list all the template revisions in a given NStemplate tier
-	if err := r.Client.List(ctx, &ttRevList, runtimeclient.InNamespace(nsTmplTier.GetNamespace()),
-		runtimeclient.MatchingLabels{toolchainv1alpha1.TierLabelKey: nsTmplTier.GetName()}); err != nil {
-		return nil, err
+	//the ttr age should be greater than 30 seconds
+	if timeSinceCreation < deletionTimeThreshold {
+		requeAfter := deletionTimeThreshold - timeSinceCreation
+		return false, true, requeAfter, fmt.Errorf("the revision creation timestamp is less than 30 sec(new revision)")
 	}
 
-	//finding the tier revisions which are not refernced/used by the nstemplatetier.status.revisions
-	for _, ttrs := range ttRevList.Items {
-		unUsedRevisions[ttrs.Name] = true
-		for _, revCR := range nsTmplTier.Status.Revisions {
-			if ttrs.Name == revCR {
-				unUsedRevisions[ttrs.Name] = false
-			}
+	//check if the ttr name is present status.revisions
+	for _, ttstatusrev := range nsTmplTier.Status.Revisions {
+		if ttstatusrev == rev.Name {
+			return false, false, 0, fmt.Errorf("the revision is still being referenced in status.revisions")
 		}
 	}
 
-	//list the not refernced/unused tier revisions whose creation time stamp is older than 1 day.
-	for _, ttrs := range ttRevList.Items {
-		for ttrname, value := range unUsedRevisions {
-			if ttrs.Name == ttrname && value {
-				ttCreateTime, err := time.Parse(time.RFC3339, ttrs.CreationTimestamp.String())
-				if err != nil {
-					return nil, err
-				}
-				duration := -time.Until(ttCreateTime)
-				if duration > ageOld {
-					ttrDelList = append(ttrDelList, ttr)
-				}
-			}
-		}
+	// get the outdated matchig label to list outdated spaces
+	matchOutdated, err := nstemplatetier.OutdatedTierSelector(nsTmplTier)
+	if err != nil {
+		return false, false, 0, err
+
+	}
+	// look-up all spaces associated with the NSTemplateTier which are outdated
+	spaces := &toolchainv1alpha1.SpaceList{}
+	if err := r.Client.List(ctx, spaces, runtimeclient.InNamespace(nsTmplTier.Namespace),
+		matchOutdated, runtimeclient.Limit(1)); err != nil {
+		return false, false, 0, err
 	}
 
-	return ttrDelList, nil
-}
-
-func (r *Reconciler) DeleteRevision(ctx context.Context, ttrevisions []toolchainv1alpha1.TierTemplateRevision) error {
-	for _, revision := range ttrevisions {
-		if err := r.Client.Delete(ctx, &revision); err != nil {
-			return fmt.Errorf("%s was not deleted : %w", revision.Name, err)
-		}
+	//there should not be any outdated spaces
+	if len(spaces.Items) > 0 {
+		return false, false, 0, fmt.Errorf("there are still some spaces which are outdated")
 	}
-	return nil
+
+	return true, false, 0, nil
+
 }
--- controllers/nstemplatetierrevisioncleanup/nstemplatetier_revision_cleanup_controller_test.go
+++ controllers/nstemplatetierrevisioncleanup/nstemplatetier_revision_cleanup_controller_test.go
@@ -1,79 +1,229 @@
-package nstemplatetierrevisioncleanup
+package nstemplatetierrevisioncleanup_test
 
 import (
+	"context"
 	"fmt"
-	"strings"
+	"os"
 	"testing"
+	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	"github.com/codeready-toolchain/host-operator/controllers/nstemplatetierrevisioncleanup"
 	"github.com/codeready-toolchain/host-operator/pkg/apis"
+	tiertest "github.com/codeready-toolchain/host-operator/test/nstemplatetier"
+	"github.com/codeready-toolchain/host-operator/test/tiertemplaterevision"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
-	templatev1 "github.com/openshift/api/template/v1"
+	spacetest "github.com/codeready-toolchain/toolchain-common/pkg/test/space"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubectl/pkg/scheme"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 const (
 	operatorNamespace = "toolchain-host-operator"
 )
 
-func TestReconcile(t *testing.T) {
+func TestTTRDeletionReconcile(t *testing.T) {
+	//given
+	nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+	ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+	s := createSpace(nsTemplateTier)
+	r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+	//when
+	res, err := r.Reconcile(context.TODO(), req)
+	//then
+	require.NoError(t, err)
+	require.Equal(t, controllerruntime.Result{}, res)
+	tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).DoNotExist()
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("the creation timestamp is less than 30 sec", func(t *testing.T) {
+			// given
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.PreviousBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttr"), metav1.NewTime(time.Now()))
+			s := createSpace(nsTemplateTier)
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "cannot delete the current TTR,requing :the revision creation timestamp is less than 30 sec(new revision)")
+			require.Equal(t, true, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+
+		})
+		t.Run("ttr is still being referenced in status.revisions", func(t *testing.T) {
+			// given
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.PreviousBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			s := createSpace(nsTemplateTier)
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "cannot delete the current TTR,requing :the revision is still being referenced in status.revisions")
+			require.Equal(t, false, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+
+		})
+
+		t.Run("spaces are still being updated", func(t *testing.T) {
+			// given
+			nsTemplateTier.Status.Revisions = map[string]string{
+				"base1ns-code-123456new":             "base1ns-code-123456new-ttr",
+				"base1ns-clusterresources-123456new": "base1ns-clusterresources-123456new-ttr",
+			}
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "cannot delete the current TTR,requing :there are still some spaces which are outdated")
+			require.Equal(t, false, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+		})
+		t.Run("Error while deleting the TTR", func(t *testing.T) {
+			// given
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			s := createSpace(nsTemplateTier)
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+			cl.MockDelete = func(ctx context.Context, obj runtimeclient.Object, opts ...runtimeclient.DeleteOption) error {
+				return fmt.Errorf("some error cannot delete")
+			}
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "unable to delete the current Tier Template Revision base1ns-clusterresources-123456new-ttrcr: some error cannot delete")
+			require.Equal(t, false, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+
+		})
+
+		t.Run("error while getting revision", func(t *testing.T) {
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			s := createSpace(nsTemplateTier)
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+			cl.MockGet = func(ctx context.Context, key runtimeclient.ObjectKey, obj runtimeclient.Object, opts ...runtimeclient.GetOption) error {
+				return fmt.Errorf("some error cannot get")
+			}
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "unable to get the current TierTemplateRevision: some error cannot get")
+			require.Equal(t, false, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+		})
+
+		t.Run("error while listing outdated spaces", func(t *testing.T) {
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			s := createSpace(nsTemplateTier)
+			r, req, cl := prepareReconcile(t, ttr.Name, ttr, s, nsTemplateTier)
+			cl.MockList = func(ctx context.Context, list runtimeclient.ObjectList, opts ...runtimeclient.ListOption) error {
+				return fmt.Errorf("some error")
+			}
+			// when
+			res, err := r.Reconcile(context.TODO(), req)
+
+			// then
+			require.EqualError(t, err, "cannot delete the current TTR,requing :some error")
+			require.Equal(t, false, res.Requeue)
+			tiertemplaterevision.AssertThatTTRs(t, cl, nsTemplateTier.GetNamespace()).ExistFor(nsTemplateTier.Name)
+		})
+
+		t.Run("revision not found", func(t *testing.T) {
+			r, req, _ := prepareReconcile(t, "base")
+			//when
+			res, err := r.Reconcile(context.TODO(), req)
+			//then
+			require.NoError(t, err)
+			require.Equal(t, false, res.Requeue)
+
+		})
+
+		t.Run("NsTemplate Tier not found", func(t *testing.T) {
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			r, req, _ := prepareReconcile(t, ttr.Name, ttr)
+			//when
+			res, err := r.Reconcile(context.TODO(), req)
+			//then
+			require.NoError(t, err)
+			require.Equal(t, false, res.Requeue)
+
+		})
+
+		t.Run("tier label not found", func(t *testing.T) {
+			nsTemplateTier := tiertest.Base1nsTier(t, tiertest.CurrentBase1nsTemplates, tiertest.WithStatusRevisions())
+			ttr := createttr(*nsTemplateTier, (nsTemplateTier.Spec.ClusterResources.TemplateRef + "-ttrcr"), metav1.NewTime(time.Now().Add(-time.Minute)))
+			ttr.Labels = map[string]string{}
+			r, req, _ := prepareReconcile(t, ttr.Name, ttr)
+			//when
+			res, err := r.Reconcile(context.TODO(), req)
+			//then
+			require.EqualError(t, err, "tier-name label not found in tiertemplaterevision")
+			require.Equal(t, false, res.Requeue)
+
+		})
+
+	})
 
 }
 
-func createTierTemplate(t *testing.T, typeName string, withTemplateObjects []runtime.RawExtension, tierName string) *toolchainv1alpha1.TierTemplate {
-	var (
-		ns test.TemplateObject = `
-- apiVersion: v1
-  kind: Namespace
-  metadata:
-    name: ${SPACE_NAME}
-`
-		spacename test.TemplateParam = `
-- name: SPACE_NAME
-  value: johnsmith`
-	)
+func prepareReconcile(t *testing.T, name string, initObjs ...runtimeclient.Object) (*nstemplatetierrevisioncleanup.Reconciler, reconcile.Request, *test.FakeClient) {
+	os.Setenv("WATCH_NAMESPACE", test.HostOperatorNs)
 	s := scheme.Scheme
 	err := apis.AddToScheme(s)
 	require.NoError(t, err)
-	codecFactory := serializer.NewCodecFactory(s)
-	decoder := codecFactory.UniversalDeserializer()
-	tmpl := templatev1.Template{}
-	_, _, err = decoder.Decode([]byte(test.CreateTemplate(test.WithObjects(ns), test.WithParams(spacename))), nil, &tmpl)
-	require.NoError(t, err)
-
-	revision := "123456new"
-	// we can set the template field to something empty as it is not relevant for the tests
-	tt := &toolchainv1alpha1.TierTemplate{
+	cl := test.NewFakeClient(t, initObjs...)
+	r := &nstemplatetierrevisioncleanup.Reconciler{
+		Client: cl,
+		Scheme: s,
+	}
+	return r, reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: operatorNamespace,
+		},
+	}, cl
+}
+func createttr(nsTTier toolchainv1alpha1.NSTemplateTier, name string, crtime metav1.Time) *toolchainv1alpha1.TierTemplateRevision {
+	labels := map[string]string{
+		toolchainv1alpha1.TierLabelKey: nsTTier.Name,
+	}
+	ttr := &toolchainv1alpha1.TierTemplateRevision{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      strings.ToLower(fmt.Sprintf("%s-%s-%s", tierName, typeName, revision)),
-			Namespace: test.HostOperatorNs,
+			Namespace:         operatorNamespace,
+			Name:              name,
+			Labels:            labels,
+			CreationTimestamp: crtime,
 		},
-		Spec: toolchainv1alpha1.TierTemplateSpec{
-			TierName: tierName,
-			Type:     typeName,
-			Revision: revision,
-			Template: tmpl,
+		Spec: toolchainv1alpha1.TierTemplateRevisionSpec{
+			Parameters: nsTTier.Spec.Parameters,
 		},
 	}
-
-	// just copy the raw objects to the templateObjects field
-	// TODO this will be removed once we switch on using templateObjects only in the TierTemplates
-	if withTemplateObjects != nil {
-		tt.Spec.TemplateObjects = withTemplateObjects
-	}
-
-	return tt
+	return ttr
 }
 
-func withTemplateObjects(templates ...unstructured.Unstructured) []runtime.RawExtension {
-	var templateObjects []runtime.RawExtension
-	for i := range templates {
-		templateObjects = append(templateObjects, runtime.RawExtension{Object: &templates[i]})
-	}
-	return templateObjects
+func createSpace(nsTTier *toolchainv1alpha1.NSTemplateTier) *toolchainv1alpha1.Space {
+	testSpace := spacetest.NewSpace(test.HostOperatorNs, "oddity1",
+		spacetest.WithTierNameAndHashLabelFor(nsTTier),
+		spacetest.WithSpecTargetCluster("member-1"),
+		spacetest.WithStatusTargetCluster("member-1"),
+		spacetest.WithFinalizer(),
+		spacetest.WithCondition(spacetest.Ready()))
+	return testSpace
 }
