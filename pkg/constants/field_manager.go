c00d530cbf58c03d5cff5d271a9b600c510fa73c
Lukas Krejci <metlos@users.noreply.github.com>
2025-05-02 15:57:39 +0200

    Deploy the registration service using the new SSA client (#1171)

---
+++ pkg/constants/field_manager.go
@@ -0,0 +1,5 @@
+package constants
+
+// HostOperatorFieldManager is the field manager we want to use in the managed fields
+// of objects deployed by the host operator.
+const HostOperatorFieldManager = "kubesaw-host-operator"
