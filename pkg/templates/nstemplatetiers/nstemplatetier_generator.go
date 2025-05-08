14f52bdbc708ce83aef49caa4ec7dee69b7f199c
Feny Mehta <fbm3307@gmail.com>
2025-05-08 17:37:22 +0530

    just for test purpose
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- pkg/templates/nstemplatetiers/nstemplatetier_generator.go
+++ pkg/templates/nstemplatetiers/nstemplatetier_generator.go
@@ -40,7 +40,7 @@ func CreateOrUpdateResources(ctx context.Context, s *runtime.Scheme, client runt
 		files[name] = content
 	}
 
-	// initialize tier generator, loads templates from assets
+	// initialize tier generator, loads templates from assets .
 	return nstemplatetiers.GenerateTiers(s, func(toEnsure runtimeclient.Object, canUpdate bool, _ string) (bool, error) {
 		if !canUpdate {
 			if err := client.Create(ctx, toEnsure); err != nil && !apierrors.IsAlreadyExists(err) {
