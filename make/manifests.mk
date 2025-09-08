e45c03e65d95388f64af1fb05495d975bc0d4ed2
Rafaela Soares <rsoaresd@redhat.com>
2025-09-08 13:11:11 +0100

    clean

--- make/manifests.mk
+++ make/manifests.mk
@@ -46,7 +46,6 @@ bundle: clean-bundle generate-rbac kustomize ## Generate bundle manifests and me
 	operator-sdk generate kustomize manifests -q
 	$(eval TMP_MANIFEST_FILE := $(shell mktemp))
 	@echo "generating manifests to temporary file: ${TMP_MANIFEST_FILE}"
-	echo $(KUSTOMIZE)
 	$(KUSTOMIZE) build config/manifests -o ${TMP_MANIFEST_FILE}
 	cat ${TMP_MANIFEST_FILE} | operator-sdk generate bundle --overwrite --version=${NEXT_VERSION} --channels ${CHANNEL} --default-channel ${CHANNEL} --package toolchain-host-operator
 	operator-sdk bundle validate ./bundle
