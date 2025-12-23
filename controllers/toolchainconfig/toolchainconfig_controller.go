29d520b1117f55de9ce36bb9d09ab30ad13f7bee
MikelAlejoBR <mbarcina@redhat.com>
2025-12-23 10:05:11 -0500

    feature: debug builds for the host operator
    In order to be able to debug the operator live in the local cluster, we
    need the binary to be executed with Delve instead, and we need it to be
    built without any code optimizations or minimization so that the
    breakpoints work.
    
    For that purpose, a new Dockerfile is added which can build the image
    that way, and the corresponding Make targets make it easy to kick off
    the whole process with a single command.
    
    The Makefile targets also enable the "toolchain-e2e" repository to
    easily build the "debug" image.
    
    Also, when Developer Sandbox is deployed locally, usually the
    registration service gets managed by the "ToolChain config controller",
    which launches it with a specific command. In order to be able to launch
    the registration service with Delve, a few minor modifications are made
    so that that launch command can be overridden.
    
    SANDBOX-1561

--- controllers/toolchainconfig/toolchainconfig_controller.go
+++ controllers/toolchainconfig/toolchainconfig_controller.go
@@ -151,6 +151,16 @@ func getVars(namespace string, cfg ToolchainConfig) templateVars {
 	vars["IMAGE"] = image
 	vars.addIfNotEmpty("NAMESPACE", namespace)
 	vars.addIfNotEmpty("REPLICAS", fmt.Sprint(cfg.RegistrationService().Replicas()))
+
+	// Allow overriding the registration service's command via an environment
+	// variable.
+	command := os.Getenv(RegistrationServiceCommandEnvKey)
+	if command != "" {
+		vars["COMMAND"] = command
+	} else {
+		vars["COMMAND"] = `["registration-service"]`
+	}
+
 	return vars
 }
 
--- make/podman.mk
+++ make/podman.mk
@@ -10,9 +10,19 @@ IMAGE_PLATFORM ?= linux/amd64
 podman-image: build
 	$(Q)podman build --platform ${IMAGE_PLATFORM} -f build/Dockerfile -t ${IMAGE} .
 
+## Build the operator's image with Delve on it so that it is ready to attach a
+## debugger to it.
+podman-image-debug: build-debug
+	$(Q) podman build --platform ${IMAGE_PLATFORM} --build-arg GOLANG_VERSION="$$(go version | awk '{print $$3}')" --file build/Dockerfile.debug --tag ${IMAGE} .
+
 .PHONY: podman-push
 ## Push the binary image to quay.io registry
 podman-push: check-namespace podman-image
+	$(Q)podman push ${IMAGE}
+
+.PHONY: podman-push-debug
+## Push the image with the debugger in it to the repository.
+podman-push-debug: check-namespace podman-image-debug
 	$(Q)podman push ${IMAGE}
 
 .PHONY: check-namespace
